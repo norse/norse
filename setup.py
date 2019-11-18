34f16afa9e26d03d956e5482f018dd212016fef2
Christian Pehle <christian.pehle@gmail.com>
2019-11-18 12:54:56 +0100

    Point to public version of the repository

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=install_requires,
     name="myelin",
     description="A library for deep learning with spiking neural networks",
-    url="http://github.com/cpehle/myelin",
+    url="http://github.com/electronicvisions/myelin",
     author="Christian Pehle",
     author_email="christian.pehle@gmail.com",
     packages=setuptools.find_packages(),
