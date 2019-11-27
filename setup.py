abfe0c9bf3c858e8cdd676567bcb047bfb946b48
Jens Egholm <jensegholm@protonmail.com>
2019-11-27 14:40:04 +0100

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 import setuptools
 from setuptools import setup
 
-with open('requirements.txt') as fp:
+from os import path
+pwd = path.abspath(path.dirname(__file__))
+
+with open(path.join(pwd, 'requirements.txt')) as fp:
     install_requires = fp.read()
+
+with open(path.join(pwd, 'Readme.md'), encoding='utf-8') as fp:
+    readme_text = fp.read()
 
 setup(
     install_requires=install_requires,
-    name="norse",
-    description="A library for deep learning with spiking neural networks",
-    url="http://github.com/electronicvisions/norse",
-    author="Christian Pehle",
-    author_email="christian.pehle@gmail.com",
+    name='norse',
+    description='A library for deep learning with spiking neural networks',
+    long_description=readme_text,
+    long_description_content_type='text/markdown',
+    url='http://github.com/electronicvisions/norse',
+    author='Christian Pehle',
+    author_email='christian.pehle@gmail.com',
     packages=setuptools.find_packages(),
 )
