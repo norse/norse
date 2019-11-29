e735eb531f41f0477b667cbc995625ed31c64e36
Christian Pehle <christian.pehle@gmail.com>
2019-11-30 00:22:38 +0100

    Add classifiers and keywords to setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(pwd, 'Readme.md'), encoding='utf-8') as fp:
 setup(
     install_requires=install_requires,
     name='norse',
-    version = '0.0.1',
+    version = '0.0.2',
     description='A library for deep learning with spiking neural networks',
     long_description=readme_text,
     long_description_content_type='text/markdown',
@@ -21,4 +21,19 @@ setup(
     author='Christian Pehle',
     author_email='christian.pehle@gmail.com',
     packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='norse machine learning',
 )
