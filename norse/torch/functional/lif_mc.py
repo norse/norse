ad796a9504369ce534b34d563744ac9047c7d8ce
Christian Pehle <christian.pehle@gmail.com>
2020-05-16 16:17:13 +0200

    Refactor lif_mc

--- norse/torch/functional/lif_mc.py
+++ norse/torch/functional/lif_mc.py
@@ -31,32 +31,10 @@ def lif_mc_step(
         p (LIFParameters): neuron parameters
         dt (float): Integration timestep to use
     """
-<<<<<<< HEAD
     state.v = state.v + dt * torch.nn.functional.linear(state.v, g_coupling)
     return lif_step(
         input_tensor, state, input_weights, recurrent_weights, parameters, dt
     )
-=======
-    # compute voltage
-    dv = dt * parameters.tau_mem_inv * (
-        (parameters.v_leak - state.v) + state.i
-    ) + torch.nn.functional.linear(state.v, g_coupling)
-    v_decayed = state.v + dv
-    # compute current updates
-    di = -dt * parameters.tau_syn_inv * state.i
-    i_decayed = state.i + di
-    # compute new spikes
-    z_new = threshold(v_decayed - parameters.v_th, parameters.method, parameters.alpha)
-    # compute reset
-    v_new = (1 - z_new) * v_decayed + z_new * parameters.v_reset
-    # compute current jumps
-    i_new = (
-        i_decayed
-        + torch.nn.functional.linear(input_tensor, input_weights)
-        + torch.nn.functional.linear(state.z, recurrent_weights)
-    )
-    return z_new, LIFState(z_new, v_new, i_new)
->>>>>>> 6b7bb4790156f9744373086e6e8df96dbb0dfeb8
 
 
 def lif_mc_feed_forward_step(
@@ -77,23 +55,5 @@ def lif_mc_feed_forward_step(
         p (LIFParameters): neuron parameters
         dt (float): Integration timestep to use
     """
-<<<<<<< HEAD
     state.v = state.v + dt * torch.nn.functional.linear(state.v, g_coupling)
     return lif_feed_forward_step(input_tensor, state, parameters, dt)
-=======
-    # compute voltage
-    dv = dt * parameters.tau_mem_inv * (
-        (parameters.v_leak - state.v) + state.i
-    ) + torch.nn.functional.linear(state.v, g_coupling)
-    v_decayed = state.v + dv
-    # compute current updates
-    di = -dt * parameters.tau_syn_inv * state.i
-    i_decayed = state.i + di
-    # compute new spikes
-    z_new = threshold(v_decayed - parameters.v_th, parameters.method, parameters.alpha)
-    # compute reset
-    v_new = (1 - z_new) * v_decayed + z_new * parameters.v_reset
-    # compute current jumps
-    i_new = i_decayed + input_tensor
-    return z_new, LIFFeedForwardState(v_new, i_new)
->>>>>>> 6b7bb4790156f9744373086e6e8df96dbb0dfeb8
