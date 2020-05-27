57ae0091484f414c067c2e852de9f8f1bd44f73b
Christian Pehle <christian.pehle@gmail.com>
2020-05-27 12:34:36 +0200

    Modify bindsnet benchmark

--- norse/benchmark/bindsnet_lif.py
+++ norse/benchmark/bindsnet_lif.py
@@ -4,7 +4,9 @@ from absl import logging
 
 import csv, time, torch
 
-from bindsnet.network.nodes import LIFNodes
+from bindsnet.network import Network
+from bindsnet.network.topology import Connection
+from bindsnet.network.nodes import LIFNodes, Input
 from bindsnet.encoding import PoissonEncoder
 
 FLAGS = flags.FLAGS
@@ -19,7 +21,7 @@ flags.DEFINE_integer("start", 250, "Start of the number of inputs to sweep")
 flags.DEFINE_integer("step", 250, "Steps in which to sweep over the number of inputs")
 flags.DEFINE_integer("stop", 10000, "Number of inputs to sweep to")
 flags.DEFINE_integer("sequence_length", 1000, "Number of timesteps to simulate")
-flags.DEFINE_float("dt", 0.01, "Simulation timestep")
+flags.DEFINE_float("dt", 0.001, "Simulation timestep")
 flags.DEFINE_string("device", "cpu", "Device to use [cpu, cuda]")
 
 
@@ -31,33 +33,31 @@ def lif_feed_forward_benchmark(
     dt=0.001,
     device="cpu",
 ):
-    fc = torch.nn.Linear(input_features, output_features, bias=False).to(device)
-    T = n_time_steps
+    T = dt * n_time_steps
+    network = Network(batch_size=batch_size, dt=dt)
+
+    network.add_layer(Input(n=input_features), name="Input")
+    network.add_layer(LIFNodes(n=output_features), name="Neurons")
+    network.add_connection(
+        Connection(source=network.layers["Input"], target=network.layers["Neurons"]),
+        source="Input",
+        target="Neurons",
+    )
+
     input_spikes = (
-        PoissonEncoder(time=n_time_steps, dt=dt)(
+        PoissonEncoder(time=T, dt=dt)(
             0.3 * torch.ones(batch_size, input_features)
         )
         .to(device)
         .float()
     )
-    model = LIFNodes(
-        n=output_features,
-        learning=False,
-        dt=dt,
-    ).to(device)
-    model.set_batch_size(batch_size)
-    model.dt = dt
 
+    input_data = {"Input": input_spikes}
     start = time.time()
-
-    spikes = []
-    for ts in range(T):
-        x = fc(input_spikes[ts, :])
-        model(x)
-        spikes += [model.s]
-
-    spikes = torch.stack(spikes)
+    network.to(device)
+    network.run(inputs=input_data, time=T)
     end = time.time()
+
     duration = end - start
 
     result = {
