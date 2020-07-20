6a4f56cb996e062b116d64449102297494bcebdc
Christian Pehle <christian.pehle@gmail.com>
2020-07-20 23:50:52 +0200

    Add temporal integration for lif neuron

--- norse/csrc/op.cpp
+++ norse/csrc/op.cpp
@@ -29,14 +29,14 @@ torch::Tensor superfun(torch::Tensor input, torch::Tensor alpha) {
   return SuperFunction::apply(input, alpha);
 }
 
-auto lif_super_step(torch::Tensor input_tensor,
-                    std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> s,
-                    torch::Tensor input_weights,
-                    torch::Tensor recurrent_weights,
-                    std::tuple<torch::Tensor, torch::Tensor, torch::Tensor,
-                               torch::Tensor, torch::Tensor, torch::Tensor>
-                        p,
-                    double dt)
+template <torch::Tensor f(torch::Tensor, torch::Tensor)>
+auto lif_step(torch::Tensor input_tensor,
+              std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> s,
+              torch::Tensor input_weights, torch::Tensor recurrent_weights,
+              std::tuple<torch::Tensor, torch::Tensor, torch::Tensor,
+                         torch::Tensor, torch::Tensor, torch::Tensor>
+                  p,
+              double dt)
     -> std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> {
   auto [z, v, i] = s;
   auto [v_leak, v_th, v_reset, tau_mem_inv, tau_syn_inv, alpha] = p;
@@ -48,7 +48,7 @@ auto lif_super_step(torch::Tensor input_tensor,
   auto i_decayed = i + di;
 
   // compute new spikes
-  auto z_new = superfun(v_decayed - v_th, alpha);
+  auto z_new = f(v_decayed - v_th, alpha);
   // compute reset
   auto v_new = (1 - z_new) * v_decayed + z_new * v_reset;
   // compute current jumps
@@ -59,7 +59,34 @@ auto lif_super_step(torch::Tensor input_tensor,
   return {z_new, v_new, i_new};
 }
 
+auto lif_super_step = lif_step<superfun>;
+
+template <torch::Tensor f(torch::Tensor, torch::Tensor)>
+auto lif_integral(torch::Tensor input_tensor,
+                  std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> s,
+                  torch::Tensor input_weights, torch::Tensor recurrent_weights,
+                  std::tuple<torch::Tensor, torch::Tensor, torch::Tensor,
+                             torch::Tensor, torch::Tensor, torch::Tensor>
+                      p,
+                  double dt)
+    -> std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> {
+  auto time_steps = input_tensor.size(0);
+
+  std::vector<torch::Tensor> spikes;
+
+  for (int64_t ts = 0; ts < time_steps; ts++) {
+    auto input = input_tensor.index({ts});
+    s = lif_step<f>(input, s, input_weights, recurrent_weights, p, dt);
+    spikes.push_back(std::get<0>(s));
+  }
+
+  return {torch::stack(spikes), std::get<1>(s), std::get<2>(s)};
+}
+
+auto lif_super_integral = lif_integral<superfun>;
+
 TORCH_LIBRARY(norse_op, m) {
   m.def("superfun", superfun);
   m.def("lif_super_step", lif_super_step);
+  m.def("lif_super_integral", lif_super_integral);
 }
