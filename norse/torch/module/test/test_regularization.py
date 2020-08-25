723031c0e0cec0c1da45b0229e9b0a4d7c94cb58
Jens Egholm <jensegholm@protonmail.com>
2020-08-25 19:20:46 +0200

    Feature regularization (#104)
    Added regularization. Fixes #32

---
+++ norse/torch/module/test/test_regularization.py
@@ -0,0 +1,17 @@
+import torch
+from .. import lif
+from ..regularization import RegularizationCell
+
+
+def regularization_module_test():
+    cell = lif.LIFFeedForwardCell((2,))  # 2 -> 4
+    r = RegularizationCell()  # Defaults to spike counting
+    data = torch.ones(5, 2) + 10  # Batch size of 5
+    s = cell.initial_state(5, device="cpu")
+    z, s = cell(data, s)
+    z, rs = r(z, s)
+    assert rs == 0
+    z, s = cell(data, s)
+    z, rs = r(z, s)
+    assert rs == 10
+    assert r.state == 10
