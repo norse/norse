0f0f90c399291fe227d14d929238a5fd4483b8b5
Huizerd <jessehagenaars@gmail.com>
2020-09-03 20:35:04 +0200

    Use torch.equal

--- norse/torch/functional/test/test_regularization.py
+++ norse/torch/functional/test/test_regularization.py
@@ -9,7 +9,7 @@ def test_regularisation_spikes():
     s = LIFState(torch.ones(10), torch.ones(10), torch.ones(10))
     z, s = lif_feed_forward_step(x, s)
     zr, rs = regularize_step(z, s)
-    assert torch.all(torch.eq(z, zr))
+    assert torch.equal(z, zr)
     assert rs == 0
     z, s = lif_feed_forward_step(x, s)
     zr, rs = regularize_step(z, s)
@@ -21,5 +21,5 @@ def test_regularisation_voltage():
     s = LIFState(torch.ones(10), torch.ones(10), torch.ones(10))
     z, s = lif_feed_forward_step(x, s)
     zr, rs = regularize_step(z, s, accumulator=voltage_accumulator)
-    assert torch.all(torch.eq(z, zr))
-    assert torch.all(torch.eq(s.v, rs))
+    assert torch.equal(z, zr)
+    assert torch.equal(s.v, rs)
