cda378ea67f60bc98f1501588fe638210daf7927
Jens E. Pedersen <jensegholm@protonmail.com>
2020-09-25 14:17:11 +0200

    Added plotting, profiling and fixed norse and genn code

--- norse/benchmark/norse_lif.py
+++ norse/benchmark/norse_lif.py
@@ -10,17 +10,22 @@ from norse.torch.module.encode import PoissonEncoder
 
 from benchmark import BenchmarkParameters
 
+def no_grad(model):
+    for param in model.parameters():
+        param.requires_grad = False
+    return model
 
 def lif_feed_forward_benchmark(parameters: BenchmarkParameters):
     fc = torch.nn.Linear(parameters.features, parameters.features, bias=False).to(
         parameters.device
     )
+    no_grad(fc)
     T = parameters.sequence_length
-    p = LIFParameters(alpha=100.0, method="heaviside")
+    p = LIFParameters(method="heaviside")
     s = LIFFeedForwardState(
         v=p.v_leak,
         i=torch.zeros(
-            parameters.batch_size, parameters.features, device=parameters.device
+            parameters.batch_size, parameters.features, device=parameters.device, requires_grad=False
         ),
     )
     input_spikes = PoissonEncoder(T, dt=parameters.dt)(
@@ -29,13 +34,14 @@ def lif_feed_forward_benchmark(parameters: BenchmarkParameters):
             parameters.batch_size, parameters.features, device=parameters.device
         )
     )
+    input_spikes.requires_grad = False
     start = time.time()
 
     spikes = []
     for ts in range(T):
         x = fc(input_spikes[ts, :])
         z, s = lif_feed_forward_step(input_tensor=x, state=s, p=p, dt=parameters.dt)
-        spikes += [z]
+        spikes.append(z)
 
     spikes = torch.stack(spikes)
     end = time.time()
---
+++ norse/benchmark/plot.py
@@ -0,0 +1,25 @@
+import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+def plot_frames(frames):
+    render_frames(frames)
+    plt.show()
+
+def render_frames(frames):
+    ax = plt.gca()
+    for frame in frames:
+        print(frame.keys())
+        frame.plot(y='duration_mean', x='input_features', ax=ax, label="")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot a number of csv benchark files against each other")
+    parser.add_argument('--files', type=argparse.FileType('r'), nargs="+")
+    args = parser.parse_args()
+
+    files = args.files
+    dfs = []
+    for f in files:
+        dfs.append(pd.read_csv(f))
+    plot_frames(dfs)
