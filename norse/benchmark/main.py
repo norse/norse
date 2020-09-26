289a36f6eca01870f3518e6ba72760eaaa3efd92
y <x>
2020-09-26 13:05:07 +0200

    Added stddev to plots

--- norse/benchmark/main.py
+++ norse/benchmark/main.py
@@ -14,7 +14,7 @@ from benchmark import *
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer(
-    "batch_size", 16, "Number of data points per batch",
+    "batch_size", 64, "Number of data points per batch",
 )
 flags.DEFINE_integer("start", 250, "Start of the number of inputs to sweep")
 flags.DEFINE_integer("step", 250, "Steps in which to sweep over the number of inputs")
@@ -22,7 +22,7 @@ flags.DEFINE_integer("stop", 5000, "Number of inputs to sweep to")
 flags.DEFINE_integer("sequence_length", 1000, "Number of timesteps to simulate")
 flags.DEFINE_float("dt", 0.001, "Simulation timestep")
 flags.DEFINE_string("device", "cpu", "Device to use [cpu, cuda]")
-flags.DEFINE_integer("runs", 100, "Number of runs per simulation step")
+flags.DEFINE_integer("runs", 20, "Number of runs per simulation step")
 flags.DEFINE_bool("profile", False, "Profile Norse benchmark? (Only works for Norse)")
 
 flags.DEFINE_bool("bindsnet", True, "Benchmark Bindsnet?")
@@ -88,11 +88,11 @@ def main(argv):
     if FLAGS.bindsnet:
         import bindsnet_lif
 
-        run_benchmark(bindsnet_lif.lif_feed_forward_benchmark, "bindsnet_lif")
+        run_benchmark(bindsnet_lif.lif_feed_forward_benchmark, "BindsNET_lif")
     if FLAGS.genn:
         import genn_lif
 
-        run_benchmark(genn_lif.lif_feed_forward_benchmark, "genn_lif")
+        run_benchmark(genn_lif.lif_feed_forward_benchmark, "GeNN_lif")
     if FLAGS.norse:
         import norse_lif
 
@@ -102,14 +102,14 @@ def main(argv):
             with profiler.profile(
                 profile_memory=True, use_cuda=(FLAGS.device == "cuda")
             ) as prof:
-                run_benchmark(norse_lif.lif_feed_forward_benchmark, "norse_lif")
+                run_benchmark(norse_lif.lif_feed_forward_benchmark, "Norse_lif")
             prof.export_chrome_trace("trace.json")
         else:
-            run_benchmark(norse_lif.lif_feed_forward_benchmark, "norse_lif")
+            run_benchmark(norse_lif.lif_feed_forward_benchmark, "Norse_lif")
     if FLAGS.pysnn:
         import pysnn_lif
 
-        run_benchmark(pysnn_lif.lif_feed_forward_benchmark, "pysnn_lif")
+        run_benchmark(pysnn_lif.lif_feed_forward_benchmark, "PySNN_lif")
 
 
 def run_benchmark(function, label):
