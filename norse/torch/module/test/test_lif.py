6f63f1bdd36c92be0cfca21a6c0892971032356b
Jens Egholm <jensegholm@protonmail.com>
2020-10-01 23:52:27 +0200

    Added minor tests to ensure lif state shape

--- norse/torch/module/test/test_lif.py
+++ norse/torch/module/test/test_lif.py
@@ -6,8 +6,10 @@ from norse.torch.module.lif import LIFCell, LIFLayer, LIFFeedForwardCell
 def test_lif_cell():
     cell = LIFCell(2, 4)
     data = torch.randn(5, 2)
-    out, _ = cell(data)
+    out, s = cell(data)
 
+    for x in s:
+        assert x.shape == (5, 4)
     assert out.shape == (5, 4)
 
 
@@ -17,6 +19,20 @@ def test_lif_layer():
     out, _ = layer(data)
 
     assert out.shape == (10, 5, 4)
+
+
+def test_lif_cell_sequence():
+    l1 = LIFCell(8, 6)
+    l2 = LIFCell(6, 4)
+    l3 = LIFCell(4, 1)
+    z = torch.ones(10, 8)
+    z, s1 = l1(z)
+    z, s2 = l2(z)
+    z, s3 = l3(z)
+    assert s1.v.shape == (10, 6)
+    assert s2.v.shape == (10, 4)
+    assert s3.v.shape == (10, 1)
+    assert z.shape == (10, 1)
 
 
 def test_lif_feedforward_cell():
