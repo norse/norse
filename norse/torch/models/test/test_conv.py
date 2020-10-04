ac1f9fe3263b6b59d65b65334df5544fba40a947
Christian Pehle <christian.pehle@gmail.com>
2020-10-04 21:02:58 +0200

    Increase model coverage

---
+++ norse/torch/models/test/test_conv.py
@@ -0,0 +1,26 @@
+import torch
+import norse.torch.models.conv as conv
+
+
+def test_convnet4():
+    seq_length = 4
+    batch_size = 2
+    features = 1, 28, 28
+
+    model = conv.ConvNet4()
+    x = torch.randn(seq_length, batch_size, *features)
+
+    out = model(x)
+    assert out.shape == torch.Size([seq_length, batch_size, 10])
+
+
+def test_convnet():
+    seq_length = 4
+    batch_size = 2
+    features = 1, 28, 28
+
+    model = conv.ConvNet()
+
+    x = torch.randn(seq_length, batch_size, *features)
+    out = model(x)
+    assert out.shape == torch.Size([seq_length, batch_size, 10])
--- norse/torch/models/test/test_vgg.py
+++ norse/torch/models/test/test_vgg.py
@@ -24,6 +24,40 @@ def test_vgg11_forward_pretrained():
     assert out.shape == torch.Size([seq_length, batch_size, 1000])
 
 
+def forward(model):
+    seq_length = 2
+    batch_size = 1
+    features = 3, 256, 256
+    x = torch.randn(seq_length, batch_size, *features)
+    out = model(x)
+    assert out.shape == torch.Size([seq_length, batch_size, 1000])
+
+
+def test_vgg11_bn():
+    model = vgg.vgg11_bn()
+    forward(model)
+
+
+def test_vgg16():
+    model = vgg.vgg16()
+    forward(model)
+
+
+def test_vgg16_bn():
+    model = vgg.vgg16_bn()
+    forward(model)
+
+
+def test_vgg19():
+    model = vgg.vgg19()
+    forward(model)
+
+
+def test_vgg19_bn():
+    model = vgg.vgg19_bn()
+    forward(model)
+
+
 if __name__ == "__main__":
     test_vgg11_forward()
     test_vgg11_forward_pretrained()
