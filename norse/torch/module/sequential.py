299fd5707d6bd47716f6745d68f77130273beeee
Jens Egholm <jensegholm@protonmail.com>
2020-10-04 18:59:41 +0200

    Added tests for sequential state layer

--- norse/torch/module/sequential.py
+++ norse/torch/module/sequential.py
@@ -16,21 +16,40 @@ class SequentialState(torch.nn.Sequential):
     >>> data = torch.ones(1, 16, 8, 4)     # Single timestep
     >>> model = SequentialState(
     >>>   Lift(torch.nn.Conv2d(16, 8, 3)), # (1, 8, 6, 2)
-    >>>   torch.nn.Flatten(2)              # (1, 8, 12)
+    >>>   torch.nn.Flatten(2),             # (1, 8, 12)
     >>>   LIFLayer(12, 6),                 # (1, 8, 6)
     >>>   LIFLayer(6, 1)                   # (1, 8, 1)
     >>> )
     >>> model(data)
 
+    Example with recurrent layers:
+    >>> import torch
+    >>> from norse.torch.module.lift import Lift
+    >>> from norse.torch.module.sequential import SequentialState
+    >>> from norse.torch.module.lif import LIFCell, LIFLayer
+    >>> from norse.torch.module.lsnn import LSNNCell, LSNNLayer
+    >>> data = torch.ones(1, 16, 8, 4)     # Single timestep
+    >>> model = SequentialState(
+    >>>   Lift(torch.nn.Conv2d(16, 8, 3)), # (1, 8, 6, 2)
+    >>>   torch.nn.Flatten(2),             # (1, 8, 12)
+    >>>   LSNNLayer(LSNNCell, 12, 6),      # (1, 8, 6)
+    >>>   torch.nn.RNN(6, 4, 2),           # (1, 6, 4) with 2 recurrent layers
+    >>>   LIFLayer(LIFCell,4, 1)           # (1, 4, 1)
+    >>> )
+    >>> model(data)
+
     """
 
-    def __init__(self, *args: Any):
-        super(SequentialState, self).__init__(args)
-        # Identify all the stateful layers
+    def __init__(self, *args: torch.nn.Module):
+        super(SequentialState, self).__init__()
         self.stateful_layers = []
-        for module in self:
+        for idx, module in enumerate(args):
+            self.add_module(str(idx), module)
+            # Identify all the stateful layers
             signature = inspect.signature(module.forward)
-            self.stateful_layers.append("state" in signature.parameters)
+            self.stateful_layers.append(
+                "state" in signature.parameters or isinstance(module, torch.nn.RNNBase)
+            )
 
     def forward(self, input_tensor: torch.Tensor, state: Union[list, None] = None):
         """
