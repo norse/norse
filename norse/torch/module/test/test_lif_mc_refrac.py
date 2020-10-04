f33f50bb815739ea42ca018f85854852b66f49e1
Christian Pehle <christian.pehle@gmail.com>
2020-10-04 21:08:41 +0200

    Fix and test LIFMCRefracCell

---
+++ norse/torch/module/test/test_lif_mc_refrac.py
@@ -0,0 +1,13 @@
+import torch
+
+from norse.torch.module.lif_mc_refrac import LIFMCRefracCell
+
+
+def test_lif_mc_cell():
+    cell = LIFMCRefracCell(2, 4)
+    data = torch.randn(5, 2)
+    out, s = cell(data)
+    assert s.lif.v.shape == (5, 4)
+    assert s.lif.i.shape == (5, 4)
+    assert s.lif.z.shape == (5, 4)
+    assert out.shape == (5, 4)
