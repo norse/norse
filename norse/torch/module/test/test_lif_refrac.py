04a31ec51b8275ecbd122b08bfe9d15a8f7fba47
Christian Pehle <christian.pehle@gmail.com>
2020-10-04 21:08:19 +0200

    FIx and test LIFRefracCell

---
+++ norse/torch/module/test/test_lif_refrac.py
@@ -0,0 +1,25 @@
+import torch
+
+from norse.torch.module.lif_refrac import LIFRefracCell, LIFRefracFeedForwardCell
+
+
+def test_lif_refrac_cell():
+    cell = LIFRefracCell(2, 4)
+    data = torch.randn(5, 2)
+    out, s = cell(data)
+    assert s.rho.shape == (5, 4)
+    assert s.lif.v.shape == (5, 4)
+    assert s.lif.i.shape == (5, 4)
+    assert s.lif.z.shape == (5, 4)
+    assert out.shape == (5, 4)
+
+
+def test_lif_refrac_feedforward():
+    batch_size = 16
+    cell = LIFRefracFeedForwardCell()
+    x = torch.randn(batch_size, 20, 30)
+    out, s = cell(x)
+    assert out.shape == (batch_size, 20, 30)
+    assert s.lif.v.shape == (batch_size, 20, 30)
+    assert s.lif.i.shape == (batch_size, 20, 30)
+    assert s.rho.shape == (batch_size, 20, 30)
