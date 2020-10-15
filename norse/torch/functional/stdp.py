34e7a411359857535e471d5d470b7d70379ff281
Tushar Chauhan <research@tusharchauhan.com>
2020-10-15 22:43:50 +0200

    1. Separate STDP &  LIF 2. Tests for pot. and dep.

--- norse/torch/functional/stdp.py
+++ norse/torch/functional/stdp.py
@@ -1,53 +1,59 @@
 # Includes
 from typing import Tuple
-
 import torch
-from torch import clamp, relu, pow as exponentiate
-
 from .heaviside import heaviside
-from .lif import LIFFeedForwardState, LIFParameters, lif_feed_forward_step
-
 
 # STDP state class
 class STDPState:
-    """State of spike-timing-dependent plasticity (STDP)
+    """State of spike-timing-dependent plasticity (STDP).
     Parameters:
         t_pre (torch.Tensor): presynaptic spike trace
         t_post (torch.Tensor): postsynaptic spike trace
     """
 
-    def __init__(self, t_pre, t_post):
+    def __init__(self, t_pre: torch.Tensor, t_post: torch.Tensor):
         self.t_pre = t_pre
         self.t_post = t_post
 
     def decay(
         self,
-        z_pre,
-        z_post,
-        tau_pre_inv,
-        tau_post_inv,
-        a_pre,
-        a_post,
-        dt,
+        z_pre: torch.Tensor,
+        z_post: torch.Tensor,
+        tau_pre_inv: torch.Tensor,
+        tau_post_inv: torch.Tensor,
+        a_pre: torch.Tensor,
+        a_post: torch.Tensor,
+        dt: float = 0.001,
     ):
-        self.t_pre += dt * tau_pre_inv * (-self.t_pre + a_pre * z_pre)
-        self.t_post += dt * tau_post_inv * (-self.t_post + a_post * z_post)
-
+        """Decay function for STDP traces.
+        Parameters:
+            z_pre (torch.Tensor): presynaptic spikes
+            z_post (torch.Tensor): postsynaptic spikes
+            tau_pre_inv (torch.Tensor): inverse time-constant for the presynaptic trace
+            tau_post (torch.Tensor): inverse time-constant for the postsynaptic trace
+            a_pre (torch.Tensor): presynaptic trace
+            a_post (torch.Tensor): postsynaptic trace
+            dt (float): time-resolution
+        """
+        self.t_pre = self.t_pre + (dt * tau_pre_inv * (-self.t_pre + a_pre * z_pre))
+        self.t_post = self.t_post + (
+            dt * tau_post_inv * (-self.t_post + a_post * z_post)
+        )
 
 # STDP parameters class
 class STDPParameters:
     """STDP parameters.
     Parameters:
-        a_pre (float): Contribution of presynaptic spikes to trace
-        a_post (float): Contribution of postsynaptic spikes to trace
-        tau_pre_inv (float): Inverse decay time constant of presynaptic spike trace in 1/s
-        tau_post_inv (float): Inverse decay time constant of postsynaptic spike trace in 1/s
-        w_min (float): Lower bound on synaptic weights (should be < w_max)
-        w_max (float): Upper bound on synaptic weight (should be > w_min)
-        eta_plus (float): Learning rate for synaptic potentiation (0 < eta_plus << 1)
-        eta_minus (float): Learning rate for synaptic depression (0 < eta_minus << 1)
+        a_pre (torch.Tensor): Contribution of presynaptic spikes to trace
+        a_post (torch.Tensor): Contribution of postsynaptic spikes to trace
+        tau_pre_inv (torch.Tensor): Inverse decay time constant of presynaptic spike trace in 1/s
+        tau_post_inv (torch.Tensor): Inverse decay time constant of postsynaptic spike trace in 1/s
+        w_min (torch.Tensor): Lower bound on synaptic weights (should be < w_max)
+        w_max (torch.Tensor): Upper bound on synaptic weight (should be > w_min)
+        eta_plus (torch.Tensor): Learning rate for synaptic potentiation (0 < eta_plus << 1)
+        eta_minus (torch.Tensor): Learning rate for synaptic depression (0 < eta_minus << 1)
         stdp_algorithm (string): Algorithm for STDP updates. Options in {"additive","additive_step","multiplicative_pow","multiplicative_relu"}
-        mu (float): Exponent for multiplicative STDP (0 <= mu <= 1)
+        mu (torch.Tensor): Exponent for multiplicative STDP (0 <= mu <= 1)
         hardbound (boolean): Impose hardbounds by clipping (recommended unles eta_* << 1)
         convolutional (boolean): Convolutional weighting kernel
         stride (int): Stride for convolution
@@ -57,21 +63,21 @@ class STDPParameters:
 
     def __init__(
         self,
-        a_pre=torch.as_tensor(1.0),
-        a_post=torch.as_tensor(1.0),
-        tau_pre_inv=torch.as_tensor(1.0 / 50e-3),
-        tau_post_inv=torch.as_tensor(1.0 / 50e-3),
-        w_min=0.0,
-        w_max=1.0,
-        eta_plus=1e-3,
-        eta_minus=1e-3,
-        stdp_algorithm="additive",
-        mu=0.0,
-        hardbound=True,
-        convolutional=False,
-        stride=1,
-        padding=0,
-        dilation=1,
+        a_pre: torch.Tensor = torch.as_tensor(1.0),
+        a_post: torch.Tensor = torch.as_tensor(1.0),
+        tau_pre_inv: torch.Tensor = torch.as_tensor(1.0 / 50e-3),
+        tau_post_inv: torch.Tensor = torch.as_tensor(1.0 / 50e-3),
+        w_min: torch.Tensor = 0.0,
+        w_max: torch.Tensor = 1.0,
+        eta_plus: torch.Tensor = 1e-3,
+        eta_minus: torch.Tensor = 1e-3,
+        stdp_algorithm: str = "additive",
+        mu: torch.Tensor = 0.0,
+        hardbound: bool = True,
+        convolutional: bool = False,
+        stride: int = 1,
+        padding: int = 0,
+        dilation: int = 1,
     ):
         self.a_pre = a_pre
         self.a_post = a_post
@@ -93,65 +99,50 @@ class STDPParameters:
             self.A_minus = lambda w: self.eta_minus * heaviside(w - self.w_min)
         elif self.stdp_algorithm == "multiplicative_pow":
             self.mu = torch.tensor(mu)
-            self.A_plus = lambda w: self.eta_plus * exponentiate(
-                self.w_max - w, self.mu
-            )
-            self.A_minus = lambda w: self.eta_minus * exponentiate(
-                w - self.w_min, self.mu
-            )
+            self.A_plus = lambda w: self.eta_plus * torch.pow(self.w_max - w, self.mu)
+            self.A_minus = lambda w: self.eta_minus * torch.pow(w - self.w_min, self.mu)
         elif self.stdp_algorithm == "multiplicative_relu":
             self.mu = torch.tensor(1.0)
-            self.A_plus = lambda w: self.eta_plus * relu(self.w_max - w)
-            self.A_minus = lambda w: self.eta_minus * relu(w - self.w_min)
+            self.A_plus = lambda w: \
+                self.eta_plus * torch.nn.functional.relu(self.w_max - w)
+            self.A_minus = lambda w: \
+                self.eta_minus * torch.nn.functional.relu(w - self.w_min)
 
         # Hard bounds
         self.hardbound = hardbound
         if self.hardbound:
-            self.bounding_func = lambda w: clamp(w, w_min, w_max)
+            self.bounding_func = lambda w: torch.clamp(w, w_min, w_max)
         else:
             self.bounding_func = lambda w: w
 
         # Conv2D
         self.convolutional = convolutional
-        if convolutional:
+        if self.convolutional:
             self.stride = stride
             self.padding = padding
             self.dilation = dilation
 
-
 # %% Linear stepper
 def lif_linear_stdp_step(
     z_pre: torch.Tensor,
+    z_post: torch.Tensor,
     w: torch.Tensor,
-    state_post: LIFFeedForwardState = LIFFeedForwardState(0, 0),
-    p_post: LIFParameters = LIFParameters(),
     state_stdp: STDPState = STDPState,
     p_stdp: STDPParameters = STDPParameters(),
     dt: float = 0.001,
-) -> Tuple[torch.Tensor, LIFFeedForwardState, torch.Tensor, STDPState]:
+) -> Tuple[torch.Tensor, STDPState]:
     """STDP step for a FF LIF layer.
     Input:
-        z_pre (torch.tensor): Presynaptic activity z: {0,1} -> {no spike, spike}
+        z_pre (torch.Tensor): Presynaptic activity z: {0,1} -> {no spike, spike}
+        z_post (torch.Tensor): Postsynaptic activity z: {0,1} -> {no spike, spike}
         w (torch.Tensor): Weight tensor connecting the pre- and postsynaptic layers
-        state_post (LIFFeedForwardState): State of the postsynaptic layer before pre-synaptic input
-        p_post (LIFParameters): Parameters of the postsynaptic layer
         state_stdp (STDPState): STDP state
         p_stdp (STDPParameters): Parameters of STDP
         dt (float): Time-resolution
     Output:
-        z_post (torch.tensor): Postsynaptic activity z: {0,1} -> {no spike, spike}
-        state_post (LIFFeedForwardState): Final state of the postsynaptic layer
         w (torch.tensor): Updated synaptic weights
         state_stdp (STDPState): Updated STDP state
     """
-
-    # Update post-synaptic layer
-    z_post, state_post = lif_feed_forward_step(
-        torch.nn.functional.linear(z_pre, w),  # Integrate into LIF FF class
-        state_post,
-        p_post,
-        dt,
-    )
 
     # Update STDP traces
     state_stdp.decay(
@@ -168,53 +159,35 @@ def lif_linear_stdp_step(
     dw_plus = p_stdp.A_plus(w) * torch.einsum("bi,bj->ij", z_post, state_stdp.t_pre)
     dw_minus = p_stdp.A_minus(w) * torch.einsum("bi,bj->ij", state_stdp.t_post, z_pre)
 
-    w += dw_plus - dw_minus
+    w = w + (dw_plus - dw_minus)
 
     # Bound checking
     w = p_stdp.bounding_func(w)
 
-    return (z_post, state_post, w, state_stdp)
+    return (w, state_stdp)
 
 
 # %% Conv2D stepper
 def lif_conv2d_stdp_step(
     z_pre: torch.Tensor,
+    z_post: torch.Tensor,
     w: torch.Tensor,
-    state_post: LIFFeedForwardState = LIFFeedForwardState(0, 0),
-    p_post: LIFParameters = LIFParameters(),
     state_stdp: STDPState = STDPState,
     p_stdp: STDPParameters = STDPParameters(),
     dt: float = 0.001,
-) -> Tuple[torch.Tensor, LIFFeedForwardState, torch.Tensor, STDPState]:
+) -> Tuple[torch.Tensor, STDPState]:
     """STDP step for a conv2d LIF layer.
     Input:
         z_pre (torch.tensor): Presynaptic activity z: {0,1} -> {no spike, spike}
+        z_post (torch.tensor): Postsynaptic activity z: {0,1} -> {no spike, spike}
         w (torch.Tensor): Weight tensor connecting the pre- and postsynaptic layers
-        state_post (LIFFeedForwardState): State of the postsynaptic layer before pre-synaptic input
-        p_post (LIFParameters): Parameters of the postsynaptic layer
         state_stdp (STDPState): STDP state
         p_stdp (STDPParameters): Parameters of STDP
         dt (float): Time-resolution
     Output:
-        z_post (torch.tensor): Postsynaptic activity z: {0,1} -> {no spike, spike}
-        state_post (LIFFeedForwardState): Final state of the postsynaptic layer
         w (torch.tensor): Updated synaptic weights
         state_stdp (STDPState): Updated STDP state
     """
-
-    # Update post-synaptic layer
-    z_post, state_post = lif_feed_forward_step(
-        torch.nn.functional.conv2d(
-            z_pre,
-            w,
-            stride=p_stdp.stride,
-            padding=p_stdp.padding,
-            dilation=p_stdp.dilation,
-        ),  # Integrate into LIF FF class
-        state_post,
-        p_post,
-        dt,
-    )
 
     # Update STDP traces
     state_stdp.decay(
@@ -250,27 +223,21 @@ def lif_conv2d_stdp_step(
     )
 
     # STDP weight update
-    dw_plus = (
-        p_stdp.A_plus(w)
-        * torch.einsum(
-            "bik,bjk->ij",
-            z_post.view(batch_size, out_channels, -1),
-            state_stdp_t_pre_uf,
-        ).view(w.shape)
-    )
+    dw_plus = p_stdp.A_plus(w) * torch.einsum(
+        "bik,bjk->ij",
+        z_post.view(batch_size, out_channels, -1),
+        state_stdp_t_pre_uf,
+    ).view(w.shape)
 
-    dw_minus = (
-        p_stdp.A_minus(w)
-        * torch.einsum(
-            "bik,bjk->ij",
-            state_stdp.t_post.view(batch_size, out_channels, -1),
-            z_pre_uf,
-        ).view(w.shape)
-    )
+    dw_minus = p_stdp.A_minus(w) * torch.einsum(
+        "bik,bjk->ij",
+        state_stdp.t_post.view(batch_size, out_channels, -1),
+        z_pre_uf,
+    ).view(w.shape)
 
-    w += dw_plus - dw_minus
+    w = w + (dw_plus - dw_minus)
 
     # Bound checking
     w = p_stdp.bounding_func(w)
 
-    return (z_post, state_post, w, state_stdp)
+    return (w, state_stdp)
--- norse/torch/functional/test/test_stdp.py
+++ norse/torch/functional/test/test_stdp.py
@@ -1,235 +1,232 @@
-import copy
-
+import pytest
 import torch
+
 from ...functional.stdp import (
     STDPParameters,
     STDPState,
-    lif_conv2d_stdp_step,
     lif_linear_stdp_step,
+    lif_conv2d_stdp_step,
 )
 
-from ...functional.lif import LIFFeedForwardState, LIFParameters
 
-# Testing the linear stepper
-def test_linear_stdp_stepper(bounds=False):
+def create_id(param):
+    return param[0]
 
-    # Time points
-    time = 1000
-    # Layers
-    n_batches = 3
-    pre, post = 4, 5
-    # Weights
-    w0 = torch.empty(post, pre)
-    torch.nn.init.uniform_(w0, a=0.15, b=0.85)
-
-    # Presynaptic spiking
-    z_pre = (torch.rand((time, n_batches, pre)) > 0.1).float()
-
-    # Post synaptic state + parameters
-    state_post0 = LIFFeedForwardState(
-        v=torch.zeros(n_batches, post),
-        i=torch.zeros(n_batches, post),
+# Linear STDP
+@pytest.fixture(
+    scope="function",
+    params=[
+        ["additive", 0.0],
+        ["additive_step", 0.0],
+        ["multiplicative_pow", 0.75],
+        ["multiplicative_relu", 1.0],
+    ],
+    ids=create_id,
+)
+def initialise_for_linear_stdp(request):
+    stdp_algorithm = request.param[0]
+    n_batches = 1
+    n_pre, n_post = 2, 3
+    w0 = 0.5 * torch.ones(n_post, n_pre)
+    z_pre = torch.tensor(
+        [
+            [[1.0, 1.0]],
+            [[0.0, 0.0]],
+            [[1.0, 1.0]],
+        ]
+    ).float()
+    z_post = torch.tensor(
+        [
+            [[0.0, 0.0, 0.0]],
+            [[1.0, 1.0, 1.0]],
+            [[0.0, 0.0, 0.0]],
+        ]
+    ).float()
+    state_stdp = STDPState(
+        t_pre=torch.zeros(n_batches, n_pre),
+        t_post=torch.zeros(n_batches, n_post),
     )
-    p_post = LIFParameters()
-
-    # STDP state
-    state_stdp0 = STDPState(
-        t_pre=torch.zeros(n_batches, pre),
-        t_post=torch.zeros(n_batches, post),
-    )
-
-    # Initial weights
-    print("Initial weights:")
-    print(w0)
-    print()
-
-    # Additive
     p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="additive",
-        hardbound=bounds,
+        eta_minus=1e-1,
+        eta_plus=3e-1,  # Best to check with large, asymmetric learning-rates
+        stdp_algorithm=stdp_algorithm,
+        mu=request.param[1],
+        hardbound=False,
         convolutional=False,
     )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_linear_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
-        )
-    print("Additive:")
-    print(w)
-    print()
 
-    # Additive step
-    p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="additive_step",
-        hardbound=bounds,
-        convolutional=False,
+    return (
+        stdp_algorithm,
+        n_batches, n_pre, n_post,
+        w0, z_pre, z_post,
+        state_stdp, p_stdp,
     )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_linear_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
+
+
+def test_linear_stdp_stepper(initialise_for_linear_stdp):
+    (
+        _,
+        n_batches, n_pre, n_post,
+        w, z_pre, z_post,
+        state_stdp, p_stdp,
+    ) = initialise_for_linear_stdp
+
+    n_time = z_pre.shape[0]
+
+    t_pre = 0.0
+    t_post = 0.0
+    for n_t in range(n_time):
+        w0 = w
+        w, state_stdp = lif_linear_stdp_step(
+            z_pre[n_t], z_post[n_t], w,
+            state_stdp, p_stdp,
+            dt = 0.001,
         )
-    print("Additive step:")
-    print(w)
-    print()
 
-    # Multiplicative power
-    p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="multiplicative_pow",
-        mu=0.75,
-        hardbound=bounds,
-        convolutional=False,
-    )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_linear_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
+        # Calculating the gradient for one synapse
+        t_pre += ( 
+            0.001 *
+            (p_stdp.tau_pre_inv) *
+            (-t_pre + p_stdp.a_pre * z_pre[n_t][0][0])
         )
-    print("Multiplicative pow:")
-    print(w)
-    print()
-
-    # Multiplicative relu
-    p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="multiplicative_relu",
-        hardbound=bounds,
-        convolutional=False,
-    )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_linear_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
+        t_post += (
+            0.001 *
+            (p_stdp.tau_post_inv) *
+            (-t_post + p_stdp.a_post * z_post[n_t][0][0])
         )
-    print("Multiplicative relu:")
-    print(w)
-    print()
+
+        # Check potentiation
+        if n_t == 1:
+            assert torch.allclose(
+                torch.abs(w - w0) / torch.pow(p_stdp.w_max - w0, p_stdp.mu),
+                p_stdp.eta_plus * t_pre,
+                atol=1e-6,
+                rtol=0.0,
+            )
+        # Check depression
+        if n_t == 2:
+            assert torch.allclose(
+                torch.abs(w - w0) / torch.pow(w0, p_stdp.mu),
+                p_stdp.eta_minus * t_post,
+                atol=1e-6,
+                rtol=0.0,
+            )
+
+    # Shape checks
+    assert state_stdp.t_pre.shape == (n_batches, n_pre)
+    assert state_stdp.t_post.shape == (n_batches, n_post)
+    assert w.shape == (n_post, n_pre)
 
 
-# Testing the conv2d stepper
-def test_conv2d_stdp_stepper(bounds=False):
-
-    # Time points
-    time = 1000
-    # Architecture (batches + layers + kernel)
-    n_batches = 10
+# Conv2D STDP
+@pytest.fixture(
+    scope="function",
+    params=[
+        ["additive", 0.0],
+        ["additive_step", 0.0],
+        ["multiplicative_pow", 0.75],
+        ["multiplicative_relu", 1.0],
+    ],
+    ids=create_id,
+)
+def initialise_for_conv2d_stdp(request):
+    stdp_algorithm = request.param[0]
+    n_batches = 1
     c_pre, c_post = 3, 2
     hw_pre, hw_post = (10, 10), (8, 8)
-    kern = (3, 3)
-
-    # Weights
-    w0 = torch.nn.Conv2d(c_pre, c_post, kern).weight.detach()
-    torch.nn.init.uniform_(w0, a=0.15, b=0.85)
-
-    # Presynaptic spiking
-    z_pre = (torch.rand((time, n_batches, c_pre, *hw_pre)) > 0.1).float()
-
-    # Post synaptic state + parameters
-    state_post0 = LIFFeedForwardState(
-        v=torch.zeros(n_batches, c_post, *hw_post),
-        i=torch.zeros(n_batches, c_post, *hw_post),
+    hw_kern = (3, 3)
+    w0 = torch.nn.Conv2d(c_pre, c_post, *hw_kern).weight.detach()
+    torch.nn.init.constant_(w0, 0.5)
+    z_pre = torch.stack(
+        (
+            torch.ones(n_batches, c_pre, *hw_pre),
+            torch.zeros(n_batches, c_pre, *hw_pre),
+            torch.ones(n_batches, c_pre, *hw_pre),
+        ),
+        dim=0,
     )
-    p_post = LIFParameters()
-
-    # STDP state
-    state_stdp0 = STDPState(
+    z_post = torch.stack(
+        (
+            torch.zeros(n_batches, c_post, *hw_post),
+            torch.ones(n_batches, c_post, *hw_post),
+            torch.zeros(n_batches, c_post, *hw_post),
+        ),
+        dim=0,
+    )
+    state_stdp = STDPState(
         t_pre=torch.zeros(n_batches, c_pre, *hw_pre),
         t_post=torch.zeros(n_batches, c_post, *hw_post),
     )
-
-    # Initial weights
-    print("Initial weights:")
-    print(w0[0, 0, :])
-    print()
-
-    # Additive
     p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="additive",
-        hardbound=bounds,
+        eta_minus=1e-2,
+        eta_plus=3e-2,  # Best to check with large, asymmetric learning-rates
+        stdp_algorithm=stdp_algorithm,
+        mu=request.param[1],
+        hardbound=False,
         convolutional=True,
     )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_conv2d_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
-        )
-    print("Additive:")
-    print(w[0, 0, :])
-    print()
 
-    # Additive step
-    p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="additive_step",
-        hardbound=bounds,
-        convolutional=True,
+    return (
+        stdp_algorithm,
+        n_batches, c_pre, c_post,
+        hw_pre, hw_post, hw_kern,
+        w0, z_pre, z_post,
+        state_stdp, p_stdp,
     )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_conv2d_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
-        )
-    print("Additive step:")
-    print(w[0, 0, :])
-    print()
 
-    # Multiplicative power
-    p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="multiplicative_pow",
-        mu=0.75,
-        hardbound=bounds,
-        convolutional=True,
-    )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_conv2d_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
-        )
-    print("Multiplicative pow:")
-    print(w[0, 0, :])
-    print()
 
-    # Multiplicative relu
-    p_stdp = STDPParameters(
-        eta_minus=1e-3,
-        eta_plus=1e-3,
-        stdp_algorithm="multiplicative_relu",
-        hardbound=bounds,
-        convolutional=True,
-    )
-    state_post = copy.deepcopy(state_post0)
-    state_stdp = copy.deepcopy(state_stdp0)
-    w = copy.deepcopy(w0)
-    for i in range(time):
-        _, state_post, w, state_stdp = lif_conv2d_stdp_step(
-            z_pre[i], w, state_post, p_post, state_stdp, p_stdp
+def test_conv2d_stdp_stepper(initialise_for_conv2d_stdp):
+    (
+        _,
+        n_batches, c_pre, c_post,
+        hw_pre, hw_post, hw_kern,
+        w, z_pre, z_post,
+        state_stdp, p_stdp,
+    ) = initialise_for_conv2d_stdp
+
+    n_time = z_pre.shape[0]
+
+    t_pre = 0.0
+    t_post = 0.0
+    for n_t in range(n_time):
+        w0 = w
+        w, state_stdp = lif_conv2d_stdp_step(
+            z_pre[n_t], z_post[n_t], w,
+            state_stdp, p_stdp,
+            dt = 0.001,
         )
-    print("Multiplicative relu:")
-    print(w[0, 0, :])
-    print()
+
+        # Calculating the gradient for one synapse
+        t_pre += (
+            0.001
+            * p_stdp.tau_pre_inv
+            * (-t_pre + p_stdp.a_pre * z_pre[n_t][0][0][0][0])
+        )
+        t_post += (
+            0.001
+            * p_stdp.tau_post_inv
+            * (-t_post + p_stdp.a_post * z_post[n_t][0][0][0][0])
+        )
+
+        # Check potentiation
+        if n_t == 1:
+            assert torch.allclose(
+                torch.abs(w - w0) / torch.pow(p_stdp.w_max - w0, p_stdp.mu),
+                hw_post[0] * hw_post[1] * p_stdp.eta_plus * t_pre,
+                atol=1e-6,
+                rtol=0.0,
+            )
+
+        # Check depression
+        if n_t == 2:
+            assert torch.allclose(
+                torch.abs(w - w0) / torch.pow(w0 - p_stdp.w_min, p_stdp.mu),
+                hw_post[0] * hw_post[1] * p_stdp.eta_minus * t_post,
+                atol=1e-6,
+                rtol=0.0,
+            )
+
+    # Shape checks
+    assert state_stdp.t_pre.shape == (n_batches, c_pre, *hw_pre)
+    assert state_stdp.t_post.shape == (n_batches, c_post, *hw_post)
+    assert w.shape == (c_post, c_pre, *hw_kern)
