bdd432d2c023674161aaa3f097f038e4ccd476bb
Jens E. Pedersen <jensegholm@protonmail.com>
2020-11-04 19:02:26 +0100

    Added recording to bindsnet to equalise

--- norse/benchmark/bindsnet_lif.py
+++ norse/benchmark/bindsnet_lif.py
@@ -4,40 +4,59 @@ import torch
 from bindsnet.network import Network
 from bindsnet.network.topology import Connection
 from bindsnet.network.nodes import LIFNodes, Input
-from bindsnet.encoding import PoissonEncoder
 from bindsnet.encoding import poisson
+from bindsnet.network.monitors import Monitor
 
 from benchmark import BenchmarkParameters
 
 
-def lif_feed_forward_benchmark(parameters: BenchmarkParameters):
-    T = parameters.dt * parameters.sequence_length
-    network = Network(batch_size=parameters.batch_size, dt=parameters.dt)
-
-    network.add_layer(Input(n=parameters.features), name="Input")
-    network.add_layer(LIFNodes(n=parameters.features), name="Neurons")
-    network.add_connection(
-        Connection(source=network.layers["Input"], target=network.layers["Neurons"]),
-        source="Input",
-        target="Neurons",
-    )
-
-    input_data = {
-        "Input": poisson(
-            datum=torch.rand(
-                (parameters.batch_size, parameters.features), device=parameters.device
-            ),
-            time=T * parameters.sequence_length,
-            device=parameters.device,
+class BindsNetModule(torch.nn.Module):
+    def __init__(self, parameters: BenchmarkParameters):
+        super(BindsNetModule, self).__init__()
+        network = Network(batch_size=parameters.batch_size, dt=parameters.dt)
+        lif_nodes = LIFNodes(n=parameters.features)
+        monitor = Monitor(
+            obj=lif_nodes, state_vars=("s"), time=parameters.sequence_length
         )
-    }
+        network.add_layer(Input(n=parameters.features), name="Input")
+        network.add_layer(lif_nodes, name="Neurons")
+        network.add_connection(
+            Connection(
+                source=network.layers["Input"], target=network.layers["Neurons"]
+            ),
+            source="Input",
+            target="Neurons",
+        )
+        network.add_monitor(monitor, "Monitor")
+        network.to(parameters.device)
 
-    network.to(parameters.device)
+        self.parameters = parameters
+        self.network = network
+        self.monitor = monitor
 
+    def forward(self, input_data):
+        self.network.run(
+            inputs=input_data, time=self.parameters.dt * self.parameters.sequence_length
+        )
+        return self.monitor.get("s")
+
+
+def lif_feed_forward_benchmark(parameters: BenchmarkParameters):
     with torch.no_grad():
+        network = BindsNetModule(parameters)
+        input_data = {
+            "Input": poisson(
+                datum=torch.rand(
+                    (parameters.batch_size, parameters.features), device=parameters.device
+                ).contiguous(),
+                time=parameters.sequence_length,
+                device=parameters.device,
+            )
+        }
+
         start = time.time()
-        network.run(inputs=input_data, time=T)
+        network(input_data)
         end = time.time()
 
-    duration = end - start
-    return duration
+        duration = end - start
+        return duration
--- norse/benchmark/norse_lif.py
+++ norse/benchmark/norse_lif.py
@@ -36,33 +36,33 @@ class LIFBenchmark(torch.jit.ScriptModule):
 
 
 def lif_feed_forward_benchmark(parameters: BenchmarkParameters):
-    model = LIFBenchmark(parameters).to(parameters.device)
-    input_spikes = PoissonEncoder(parameters.sequence_length, dt=parameters.dt)(
-        0.3
-        * torch.ones(
-            parameters.batch_size, parameters.features, device=parameters.device
-        )
-    )
-    p = LIFParametersJIT(
-        tau_syn_inv=torch.as_tensor(1.0 / 5e-3),
-        tau_mem_inv=torch.as_tensor(1.0 / 1e-2),
-        v_leak=torch.as_tensor(0.0),
-        v_th=torch.as_tensor(1.0),
-        v_reset=torch.as_tensor(0.0),
-        method="super",
-        alpha=torch.as_tensor(0.0),
-    )
-    s = LIFFeedForwardState(
-        v=p.v_leak,
-        i=torch.zeros(
-            parameters.batch_size,
-            parameters.features,
-            device=parameters.device,
-        ),
-    )
     with torch.no_grad():
+        model = LIFBenchmark(parameters).to(parameters.device)
+        input_spikes = PoissonEncoder(parameters.sequence_length, dt=parameters.dt)(
+            0.3
+            * torch.ones(
+                parameters.batch_size, parameters.features, device=parameters.device
+            )
+        ).contiguous()
+        p = LIFParametersJIT(
+            tau_syn_inv=torch.as_tensor(1.0 / 5e-3),
+            tau_mem_inv=torch.as_tensor(1.0 / 1e-2),
+            v_leak=torch.as_tensor(0.0),
+            v_th=torch.as_tensor(1.0),
+            v_reset=torch.as_tensor(0.0),
+            method="super",
+            alpha=torch.as_tensor(0.0),
+        )
+        s = LIFFeedForwardState(
+            v=p.v_leak,
+            i=torch.zeros(
+                parameters.batch_size,
+                parameters.features,
+                device=parameters.device,
+            ),
+        )
         start = time.time()
         model(input_spikes, p, s)
         end = time.time()
-    duration = end - start
-    return duration
+        duration = end - start
+        return duration
