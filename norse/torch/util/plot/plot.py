49de28822a38be7a595c2418f4f38c48404cd08c
Jens Egholm <jensegholm@protonmail.com>
2021-03-05 21:31:58 +0100

    Improved plotting API

--- norse/torch/util/plot/plot.py
+++ norse/torch/util/plot/plot.py
@@ -2,124 +2,181 @@
 Utilities for plotting spikes in layers over time in 2D and 3D.
 """
 
-from typing import NamedTuple, Optional
+from typing import List
+
 import matplotlib.pyplot as plt
 
 import torch
 
-_pixels_to_inches = lambda x: x * 1e-2
 
-
-def _init_axis(ax: plt.Axes, p: NamedTuple):
-    ax.set_title(p.title)
-    ax.set_xlabel(p.x_label)
-    ax.set_ylabel(p.y_label)
-    ax.set_zlabel(p.z_label)
-
-
-class PlotParameters(NamedTuple):
-    """
-    Parameters for plotting in Matplotlib.
-
-    Arguments:
-        title (str): Name of the plot. Defaults to None
-        x_label (str): X label of the plot. Defaults to None
-        y_label (str): Y label (in the horizontal plane). Defaults to None
-        z_label (str): Z label (in the vertical plane). Defaults to None
-        c_label (str): Label for the color bar. Defaults to None
-        show_colorbar (bool): Whether to display a color bar or not, if applicable. Defaults to True
-        width (int): Width of the entire plot **in pixels**
-        height (int): Height of the entire plot **in pixels**
-
-    """
-
-    title: Optional[str] = None
-    x_label: Optional[str] = None
-    y_label: Optional[str] = None
-    z_label: Optional[str] = None
-    c_label: Optional[str] = None
-    show_colorbar: bool = True
-    width: int = 640
-    height: int = 480
+def _detach_tensor(tensor: torch.Tensor):
+    if tensor.requires_grad:
+        return tensor.detach()
+    else:
+        return tensor
 
 
 def plot_heatmap_2d(
-    spikes: torch.Tensor, p: PlotParameters = PlotParameters(), **kwargs
+    data: torch.Tensor, axes: plt.Axes = None, show_colorbar: bool = False, **kwargs
 ):
     """
-    Plots a heatmat of spikes from a single layer
+    Plots a heatmat of two-dimensional data
 
     Example:
         >>> data = torch.randn(28, 28)
-        >>> plot_histogram_2d(data)
+        >>> plot_heatmap_2d(data)
+
+    .. plot::
+
+        import torch
+        from norse.torch import plot_heatmap_2d
+        data = torch.randn(28, 28)
+        plot_heatmap_2d(data)
+        plt.show()
 
     Arguments:
-        spikes (torch.Tensor): A tensor of spikes from a single layer in two dimensions.
-        p (PlotParameters): Configuration for the overall matplotlib figure.
+        data (torch.Tensor): A tensor of data to plot in two dimensions
+                             (typically spikes with time in the first dimension and
+                             neuron id in the second).
+        axes (matplotlib.axes.Axes): The matplotlib axis to plot on, if any.
+                                     Defaults to :meth:`matplotlib.pyplot.gca`
+        show_colorbar (bool): Show a colorbar (True) or not (False).
         kwargs: Specific key-value arguments to style the figure
-                fed to the :meth:`matplotlib.pyplot.imshow`_ function.
+                fed to the :meth:`matplotlib.pyplot.imshow` function.
+
+    Returns:
+        A :class:`matplotlib.axes.Axes`.
     """
-    figure = plt.figure(
-        figsize=(_pixels_to_inches(p.width), _pixels_to_inches(p.height))
-    )
-    pos = plt.imshow(spikes, interpolation="none", aspect="auto", **kwargs)
-    if p.show_colorbar:
-        figure.colorbar(pos, ax=plt.gca(), label=p.c_label)
-    return figure
+    ax = axes if axes is not None else plt.gca()
+    kwargs["aspect"] = kwargs.get("aspect", "auto")
+    kwargs["interpolation"] = kwargs.get("interpolation", "none")
+
+    pos = plt.imshow(_detach_tensor(data).T, **kwargs)
+    if show_colorbar:
+        ax.colorbar(pos, ax=ax)
+    return ax
 
 
-def plot_histogram_2d(
-    spikes: torch.Tensor, p: PlotParameters = PlotParameters(), **kwargs
-):
+def plot_histogram_2d(data: torch.Tensor, axes: plt.Axes = None, **kwargs):
     """
-    Plots a histogram of layer activity.
+    Plots a histogram of 1-dimensional data.
 
     Example:
         >>> data = torch.randn(28, 28).flatten()
         >>> plot_histogram_2d(data)
 
-    Arguments:
-        spikes (torch.Tensor): A tensor of spikes from a single layer in one dimension.
-        p (PlotParameters): Configuration for the overall matplotlib figure.
+    .. plot::
+
+        import torch
+        from norse.torch import LIF, plot_histogram_2d
+        spikes, state = LIF()(torch.ones(100, 1))
+        plot_histogram_2d(spikes)
+        plt.show()
+
+    Arguments:plot_scatter_3d(data.mean(1))
+        data (torch.Tensor): A tensor of single-dimensional data.
+        axes (matplotlib.axes.Axes): The matplotlib axis to plot on, if any.
+                                     Defaults to :meth:`matplotlib.pyplot.gca`
         kwargs: Specific key-value arguments to style the figure
-                fed to the :meth:`matplotlib.pyplot.hist`_ function.
+                fed to the :meth:`matplotlib.pyplot.hist` function.
+
+    Returns:
+        An :class:`matplotlib.axes.Axes`.
     """
-    figure = plt.figure(
-        figsize=(_pixels_to_inches(p.width), _pixels_to_inches(p.height))
-    )
-    plt.hist(spikes, bins=p.bins, density=True, **kwargs)
-    return figure
+    ax = axes if axes is not None else plt.gca()
+    kwargs["density"] = kwargs.get("density", True)
+    plt.hist(_detach_tensor(data).numpy(), **kwargs)
+    return ax
+
+
+def plot_spikes_2d(spikes: torch.Tensor, axes: plt.Axes = None, **kwargs):
+    """
+    Plots a 2D diagram of spikes. Works similar to the :meth:`plot_heatmap_2d` but
+    in black and white.
+
+    Arguments:
+        spikes (torch.Tensor): A tensor of spikes from a single layer in two dimensions.
+        axes (matplotlib.axes.Axes): The matplotlib axis to plot on, if any.
+                                     Defaults to :meth:`matplotlib.pyplot.gca`
+        kwargs: Specific key-value arguments to style the figure
+                fed to the :meth:`matplotlib.pyplot.imshow` function.
+
+    Example:
+        >>> import torch
+        >>> from norse.torch import LIF, plot_spikes_2d
+        >>> spikes, _ = LIF()(torch.randn(200, 10))
+        >>> plot_spikes_2d(spikes)
+        >>> plt.show()
+
+    .. plot::
+
+        import torch
+        from norse.torch import LIF, plot_spikes_2d
+        spikes, _ = LIF()(torch.randn(200, 10))
+        plot_spikes_2d(spikes)
+        plt.show()
+
+    Returns:
+        An :class:`matplotlib.axes.Axes` object
+    """
+    kwargs["cmap"] = kwargs.get("cmap", "binary")
+    return plot_heatmap_2d(spikes, axes=axes, **kwargs)
 
 
 def plot_scatter_3d(
-    spikes: torch.Tensor, p: PlotParameters = PlotParameters(), **kwargs
+    spikes: torch.Tensor,
+    axes: List[plt.Axes] = None,
+    show_colorbar: bool = True,
+    **kwargs
 ):
     """
-    Plots spike activity in time. If multiple layers are given, the the layers will be
+    Plots spike activity in time. If multiple layers are given, the layers will be
     shown in subplots.
-    Expects a named tensor.
+    Expects a named tensor in three dimensions (L, X, Y) or four, with time (T, L, X, Y).
 
     Example:
-        >>> data = torch.randn(3, 100, 28, 28, names=('L', 'T', 'X', 'Y'))
+        >>> distribution = torch.distributions.bernoulli.Bernoulli(torch.tensor([0.02]))
+        >>> data = distribution.sample(sample_shape=(3, 100, 10, 10)).squeeze()
+        >>> data.names = ('L', 'T', 'X', 'Y')
         >>> plot_scatter_3d(data)
+
+    .. plot::
+
+        import torch
+        from norse.torch import LIF, plot_scatter_3d
+        distribution = torch.distributions.bernoulli.Bernoulli(torch.tensor([0.02]))
+        data = distribution.sample(sample_shape=(3, 100, 10, 10)).squeeze()
+        data.names=('L', 'T', 'X', 'Y')
+        plot_scatter_3d(data)
+        plt.show()
 
     Arguments:
         spikes (torch.NamedTensor): A tensor named with four dimensions: T (time), L (layer), X, Y.
                                     Expected to be in the range :math:`[0, 1]`.
-        p (PlotParameters): Configuration for the overall matplotlib figure.
+        axes (List[plt.Axes]): A list of Axes that should have the same length as the L dimension in
+                               the spike tensor. Defaults to None, which will generate a grid for you.
+        show_colorbar (bool): Show a colorbar (True) or not (False).
         kwargs: Specific key-value arguments to style the figure
                 fed to the :meth:`matplotlib.pyplot.scatter` function.
+
+    Returns:
+        A list of :class:`matplotlib.axes.Axes`
     """
-    fig = plt.figure(figsize=(_pixels_to_inches(p.width), _pixels_to_inches(p.height)))
-    spikes = spikes.align_to(*"TLXY")
+    if len(spikes.shape) > 3:
+        spikes = _detach_tensor(spikes).align_to(*"TLXY")
+    else:
+        spikes = _detach_tensor(spikes).align_to(*"LXY").unsqueeze(0)
     T = spikes.shape[0]
     L = spikes.shape[1]
 
-    axes = []
-    for l in range(L):
-        ax = fig.add_subplot(1, L, l + 1, projection="3d")
-        axes.append(ax)
-        _init_axis(ax, p)
+    axes = [None] * L if axes is None else axes
+    for l, axis in enumerate(axes):
+        if axis is not None:
+            ax = axis
+        else:
+            ax = plt.gcf().add_subplot(1, L, l + 1, projection="3d")
+            axes[l] = ax
+
         unnamed = spikes[:, l]
         unnamed.names = None  # Unnamed tensor required for to_sparse
         s = unnamed.to_sparse().coalesce()
@@ -135,38 +192,47 @@ def plot_scatter_3d(
     return fig
 
 
-def plot_heatmap_3d(
-    spikes: torch.Tensor, p: PlotParameters = PlotParameters(), **kwargs
-):
+def plot_heatmap_3d(spikes: torch.Tensor, show_colorbar: bool = False, **kwargs):
     """
-    Plots heatmaps for several layers in time.
-    Expects a named tensor.
-    Instead of using the :meth:`matplotlib.pyplot.imshow`_ matplotlib function,
+    Plots heatmaps for some activity in several layers.
+    Expects a named tensor with names=('L', 'X', 'Y').
+    Instead of using the :meth:`matplotlib.pyplot.imshow` matplotlib function,
     we make use of the :meth:`matplotlib.pyplot.scatter` function to disperse points in 3d.
 
     Example:
-        >>> data = torch.randn(3, 100, 28, 28, names=('L', 'X', 'Y'))
+        >>> import torch
+        >>> from norse.torch import plot_heatmap_3d
+        >>> data = torch.randn(4, 28, 28, names=('L', 'X', 'Y'))
         >>> plot_heatmap_3d(data)
+        >>> plt.show()
+
+    .. plot::
+
+        import torch
+        from norse.torch import plot_heatmap_3d
+        data = torch.randn(4, 28, 28, names=('L', 'X', 'Y'))
+        plot_heatmap_3d(data)
+        plt.show()
 
     Arguments:
         spikes (torch.NamedTensor): A tensor named with four dimensions: T (time), L (layer), X, Y.
                                     Expected to be in the range :math:`[0, 1]`.
-        p (PlotParameters): Configuration for the overall matplotlib figure.
+        show_colorbar (bool): Show a colorbar (True) or not (False).
         kwargs: Specific key-value arguments to style the figure
-                fed to the :meth:`matplotlib.pyplot.scatter`_ function.
+                fed to the :meth:`matplotlib.pyplot.scatter` function.
+
+    Returns:
+        An :class:`matplotlib.axes.Axes` object
     """
-    fig = plt.figure(figsize=(_pixels_to_inches(p.width), _pixels_to_inches(p.height)))
-    spikes = spikes.align_to(*"LXY")
+    spikes = _detach_tensor(spikes).align_to(*"LXY")
     L = spikes.shape[0]
 
-    ax = fig.add_subplot(1, 1, 1, projection="3d")
-    _init_axis(ax, p)
+    ax = plt.gcf().add_subplot(1, 1, 1, projection="3d")
     unnamed = spikes + 1e-10  # Add infinitely small amount to "trigger" all pixels
     unnamed.names = None  # Unnamed tensor required for to_sparse
     s = unnamed.to_sparse().coalesce()
 
-    if "c" not in kwargs:
-        kwargs["c"] = s.values()
+    kwargs["c"] = kwargs.get("c", s.values())
 
     ax.set_title(None)
     ax.invert_yaxis()
@@ -176,6 +242,6 @@ def plot_heatmap_3d(
     ax.set_zlim([0, s.shape[1]])
     plt.xticks(range(0, L), range(1, L + 1))
     pos = ax.scatter(s.indices()[0], s.indices()[2], s.indices()[1], **kwargs)
-    if p.show_colorbar:
-        fig.colorbar(pos, ax=ax, label=p.c_label)
-    return fig
+    if show_colorbar:
+        plt.gcf().colorbar(pos, ax=ax, label=p.c_label)
+    return ax
