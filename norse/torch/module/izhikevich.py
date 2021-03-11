f466a80aeeb1bbfc684a2dd216203002ded5f5c2
Adrien Delpierre <adri.delpierre@hotmail.fr>
2021-03-11 17:30:24 +0100

    Changed IzhikevichCell class to SNNCell

--- norse/torch/module/izhikevich.py
+++ norse/torch/module/izhikevich.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Tuple
+from typing import Any, Callable, Optional, Tuple
 
 import numpy as np
 import torch
@@ -11,18 +11,17 @@ from ..functional.izhikevich import (
     izhikevich_step,
 )
 
-from from ..functional import izhikevich
+from ..functional import izhikevich
 
+from norse.torch.module.snn import SNN, SNNCell, SNNRecurrent, SNNRecurrentCell
 
-class IzhikevichCell(torch.nn.Module):
+class IzhikevichCell(SNNCell):
     """Documentation WIP
-    Cell for a Izhikevich model neuron population.
+    Module that computes a single Izhikevich neuron-model *without* recurrence and *without* time.
 
     Parameters:
-        input_size (int): Size of the input. Also known as the number of input features.
-        hidden_size (int): Size of the hidden state. Also known as the number of input features.
         p (IzhikevichParameters): Parameters of the Izhikevich neuron model.
-        dt (float): Time step to use.
+        dt (float): Time step to use. Defaults to 0.001.
     Examples:
         >>> batch_size = 16
         >>> izhikevich = IzhikevichCell(10, 20)
@@ -32,44 +31,24 @@ class IzhikevichCell(torch.nn.Module):
 
     def __init__(
         self,
-        input_size: int,
-        hidden_size: int,
-        p: IzhikevichParameters = IzhikevichParameters(),
-        spiking_method : str,
-        dt: float = 0.001,
+        spiking_method : Callable[[torch.Size], Tuple[IzhikevichState, IzhikevichParameters]],
+        #spiking method : str,
+        **kwargs
     ):
-        super(IzhikevichCell, self).__init__()
-        self.input_weights = torch.nn.Parameter(
-            torch.randn(hidden_size, input_size) * np.sqrt(2 / hidden_size)
+        super().__init__(
+            izhikevich_step,
+            self.cell_initialization,
+            spiking_method = spiking_method,
+            **kwargs
         )
-        self.recurrent_weights = torch.nn.Parameter(
-            torch.randn(hidden_size, hidden_size) * np.sqrt(2 / hidden_size)
-        )
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.p = p
-        self.dt = dt
         
-    def cell_initialisation(self, self.input_size, self.hidden_size, self.spiking_method : str):
-        shape = (input_size, hidden_size)
-        s, self.p = getattr(izhikevich, spiking_method)(shape) 
-
-    def extra_repr(self):
-        s = f"{self.input_size}, {self.hidden_size}, p={self.p}, dt={self.dt}"
-        return s
-
-    def forward(
-        self, input_tensor: torch.Tensor, state: Optional[IzhikevichState] = None
-    ) -> Tuple[torch.Tensor, IzhikevichState]:
-        return izhikevich_step(
-            input_current,
-            s,
-            p=self.p,
-            dt=self.dt,
-        )
+    def cell_initialization(self, input_tensor: torch.Tensor, spiking_method):
+        spiking_method = self.spiking_method
+        #state, self.p = getattr(izhikevich, spiking_method)(input_tensor.shape)
+        state, p = self.spiking_method(input_tensor.shape)
 
 
-class IzhikevichLayer(torch.nn.Module):
+class Izhikevich(SNN):
     """
     A neuron layer that wraps a recurrent IzhikevichCell in time such
     that the layer keeps track of temporal sequences of spikes.
