9cf72e2e90c27372ffe24e6ade906e18f810b756
Christian Pehle <christian.pehle@gmail.com>
2021-05-22 16:24:10 +0200

    Fix init for LIFRecurrent

--- norse/torch/module/lif.py
+++ norse/torch/module/lif.py
@@ -315,7 +315,7 @@ class LIFRecurrent(SNNRecurrent):
                 *dims,
                 device=input_tensor.device,
                 dtype=input_tensor.dtype,
-            ).to_sparse(),
+            ),
             v=torch.full(
                 dims,
                 self.p.v_leak.detach(),
