f8cf673535ac09bc073c6d5ad251d7b6aa274f81
jegp <jensegholm@protonmail.com>
2021-06-15 15:18:11 +0200

    Added local ACT build

--- norse/csrc/lif_test.cpp
+++ norse/csrc/lif_test.cpp
@@ -2,7 +2,6 @@
 #include <torch/torch.h>
 #include "super.h"
 #include "lif.cpp"
-#include <iostream>
 
 auto p = std::make_tuple(torch::full({1}, 1.0 / 5e-3), torch::full({1}, 1.0 / 1e-2),
                          torch::zeros({1}), torch::ones({1}),
@@ -16,11 +15,9 @@ TEST(LIF_TEST, LIF_FF)
   auto s = std::make_tuple(torch::ones({1}), torch::ones({1}));
   auto [z, v, i] = lif_feedforward_step<superfun>(t, s, p, dt);
   EXPECT_TRUE(torch::allclose(torch::zeros({4, 1}), z));
-  std::cout << v << i;
 
   s = std::make_tuple(v, i);
   auto [z1, v1, i1] = lif_feedforward_step<superfun>(t, s, p, dt);
 
-  std::cout << z1 << v1 << i1;
   EXPECT_TRUE(torch::allclose(torch::ones({4, 1}), z1));
 }
