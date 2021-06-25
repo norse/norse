7e1e98caaa879620393655146f13bef024b670de
jegp <jensegholm@protonmail.com>
2021-06-25 13:33:00 +0200

    Fixed norse op import

--- norse/__init__.py
+++ norse/__init__.py
@@ -1,18 +1,6 @@
 """Norse is a library for doing deep learning with spiking neural networks.
 """
 
-from . import benchmark, dataset, task
-from .torch import functional, models, module
+from . import benchmark, dataset, task, torch, utils
 
-import sys
-
-try:
-    import torch
-    import norse_op
-
-    setattr(sys.modules[__name__], "IS_OPS_LOADED", True)
-    del torch  # Unload torch again to allow importing norse.torch
-except ImportError or ModuleNotFoundError:
-    setattr(sys.modules[__name__], "IS_OPS_LOADED", False)
-
-__all__ = ["task", "benchmark", "dataset", "functional", "models", "module"]
+__all__ = ["benchmark", "dataset", "task", "torch", "utils"]
--- norse/utils/__init__.py
+++ norse/utils/__init__.py
@@ -8,5 +8,6 @@ try:
     import norse_op
 
     IS_OPS_LOADED = True
+    del torch  # Unload torch again to allow importing norse.torch
 except ModuleNotFoundError:
     IS_OPS_LOADED = False
