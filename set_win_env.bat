531cb4af4b4929cefd6d289dbdad67f87f5bde01
l_u_i <luis.unzueta@gmail.com>
2021-08-22 17:55:26 +0200

    Cleaned output messages in set_win_env.bat

--- set_win_env.bat
+++ set_win_env.bat
@@ -1,8 +1,8 @@
+@echo off
 SET DISTUTILS_USE_SDK=1
 SET env_path_root=C:\Program Files (x86)\Microsoft Visual Studio\2019
 SET env_path_head=VC\Auxiliary\Build\vcvars64.bat
 
-echo off
 IF EXIST "%env_path_root%\BuildTools\%env_path_head%" (
     "%env_path_root%\BuildTools\%env_path_head%"
 ) ELSE (
