e9b15fee6b31cfa74cbc748204fc3d74e08637f5
l_u_i <luis.unzueta@gmail.com>
2021-08-22 17:40:47 +0200

    Added more VS build environment options

--- set_win_env.bat
+++ set_win_env.bat
@@ -1,2 +1,22 @@
-set DISTUTILS_USE_SDK=1
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+SET DISTUTILS_USE_SDK=1
+SET env_path_root=C:\Program Files (x86)\Microsoft Visual Studio\2019
+SET env_path_head=VC\Auxiliary\Build\vcvars64.bat
+
+echo off
+IF EXIST "%env_path_root%\BuildTools\%env_path_head%" (
+    "%env_path_root%\BuildTools\%env_path_head%"
+) ELSE (
+    IF EXIST "%build_root_path%\Community\%env_path_head%" (
+        "%build_root_path%\Community\%env_path_head%"
+    ) ELSE (
+        IF EXIST "%build_root_path%\Professional\%env_path_head%" (
+            "%build_root_path%\Professional\%env_path_head%"
+        ) ELSE (
+            IF EXIST "%build_root_path%\Entreprise\%env_path_head%" (
+                "%build_root_path%\Entreprise\%env_path_head%"
+            ) ELSE (
+                echo Visual Studio 2019 required.
+            )
+        )
+    )
+)
