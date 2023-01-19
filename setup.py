72a812e17da23487878a667ad82a075ef7ad91ec
Christian Pehle <christian.pehle@gmail.com>
2023-01-20 00:54:38 +0100

    fix: change status to production

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     author_email="christian.pehle@gmail.com, jens@jepedersen.dk",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
