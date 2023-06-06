Fixed RF kernel rotation

norse/torch/module/receptive_field.py
@@ -63,7 +63,7 @@ class SpatialReceptiveField2d(torch.nn.Module):
                 in_weights = empty_weights.clone()
                 in_weights[i] = fields
                 weights.append(in_weights)
-            weights = torch.concat(weights, 1)
+            weights = torch.concat(weights, 1).permute(1, 0, 2, 3)
 
         self.conv = torch.nn.Conv2d(in_channels, self.out_channels, size, **kwargs)
         self.conv.weight = torch.nn.Parameter(weights)

norse/torch/module/test/test_receptive_field.py
@@ -11,9 +11,10 @@ def test_spatial_receptive_field():
 
 def test_spatial_receptive_field_no_aggregation():
     m = SpatialReceptiveField2d(
-        3, 2, 2, 2, 11, derivatives=0, bias=False, aggregate=False
+        3, 2, 2, 2, 7, derivatives=0, bias=False, aggregate=False, padding="same"
     )
-    assert m.conv.weight.shape == torch.Size([3, 3 * (2 * 2 + 2), 11, 11])
+    assert m.conv.weight.shape == (3 * (2 * 2 + 2), 3, 7, 7)
+    assert m(torch.empty(3, 10, 10)).shape == (3 * (2 * 2 + 2), 10, 10)
 
 
 def test_temporal_receptive_field_1d():