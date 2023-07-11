9b0ef8e40fddbe910e83d9bb6f9c8d09027d3708
jegp <jens@jepedersen.dk>
2023-07-11 21:21:20 +0200

    Adapted to nir 0.0.6

--- norse/torch/utils/export.py
+++ norse/torch/utils/export.py
@@ -4,6 +4,7 @@ import torch
 import nir
 from nirtorch import extract_nir_graph
 
+from norse.torch.module.iaf import IAFCell
 from norse.torch.module.leaky_integrator_box import LIBoxCell
 from norse.torch.module.lif_box import LIFBoxCell
 
@@ -11,31 +12,36 @@ from norse.torch.module.lif_box import LIFBoxCell
 def _extract_norse_module(module: torch.nn.Module) -> Optional[nir.NIRNode]:
     if isinstance(module, LIFBoxCell):
         return nir.LIF(
-            tau=module.p.tau_mem_inv.detach(),
-            v_th=module.p.v_th.detach(),
+            tau=1/module.p.tau_mem_inv.detach(), # Invert time constant
+            v_threshold=module.p.v_th.detach(),
             v_leak=module.p.v_leak.detach(),
             r=torch.ones_like(module.p.v_leak.detach()),
         )
     if isinstance(module, LIBoxCell):
         return nir.LI(
-            tau=module.p.tau_mem_inv.detach(),
+            tau=1/module.p.tau_mem_inv.detach(), # Invert time constant
             v_leak=module.p.v_leak.detach(),
             r=torch.ones_like(module.p.v_leak.detach()),
         )
+    if isinstance(module, IAFCell):
+        return nir.IF(
+            r=torch.ones_like(module.p.v_th.detach()),
+            v_threshold=module.p.v_th.detach(),
+        )
     elif isinstance(module, torch.nn.Linear):
         if module.bias is None: # Add zero bias if none is present
-            return nir.Linear(
+            return nir.Affine(
                 module.weight.detach(), torch.zeros(*module.weight.shape[:-1])
             )
         else:
-            return nir.Linear(module.weight.detach(), module.bias.detach())
+            return nir.Affine(module.weight.detach(), module.bias.detach())
 
     return None
 
 
 def to_nir(
     module: torch.nn.Module, sample_data: torch.Tensor, model_name: str = "norse"
-) -> nir.NIR:
+) -> nir.NIRNode:
     return extract_nir_graph(
         module, _extract_norse_module, sample_data, model_name=model_name
     )
--- norse/torch/utils/test/test_nir.py
+++ norse/torch/utils/test/test_nir.py
@@ -13,12 +13,14 @@ def test_sequential():
         torch.nn.Linear(2, 1),
     )
     graph = export.to_nir(m, torch.randn(1, 10))
-    assert len(graph.nodes) == 4
-    assert isinstance(graph.nodes[0], nir.LIF)
-    assert isinstance(graph.nodes[1], nir.Linear)
-    assert isinstance(graph.nodes[2], nir.LI)
-    assert isinstance(graph.nodes[3], nir.Linear)
-    assert len(graph.edges) == 3
+    assert len(graph.nodes) == 6 # 4 + 2 for input and output
+    assert isinstance(graph.nodes[0], nir.Input)
+    assert isinstance(graph.nodes[1], nir.LIF)
+    assert isinstance(graph.nodes[2], nir.Affine)
+    assert isinstance(graph.nodes[3], nir.LI)
+    assert isinstance(graph.nodes[4], nir.Affine)
+    assert isinstance(graph.nodes[5], nir.Output)
+    assert len(graph.edges) == 5
 
 def test_linear():
     in_features = 2
@@ -26,8 +28,8 @@ def test_linear():
     m = torch.nn.Linear(in_features, out_features, bias=False)
     m2 = torch.nn.Linear(in_features, out_features, bias=True)
     graph = export.to_nir(m, torch.randn(1, in_features))
-    assert len(graph.nodes) == 1
-    assert graph.nodes[0].weights.shape == (out_features, in_features)
-    assert graph.nodes[0].bias.shape == m2.bias.shape
+    assert len(graph.nodes) == 3
+    assert graph.nodes[1].weight.shape == (out_features, in_features)
+    assert graph.nodes[1].bias.shape == m2.bias.shape
 
 
