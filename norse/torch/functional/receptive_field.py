Updated receptive fields with correct gradients and scales

norse/torch/functional/receptive_field.py
@@ -48,7 +48,8 @@ def spatial_receptive_field(
     c = (r * sm) @ (sm * r).T
     xs, ys = torch.meshgrid(a, a, indexing="xy")
     coo = torch.stack([xs, ys], dim=2)
-    return gaussian_kernel(coo, scale, c)
+    k = gaussian_kernel(coo, scale, c)
+    return k / k.sum()
 
 
 def _extract_derivatives(

norse/torch/module/receptive_field.py
@@ -99,10 +99,15 @@ class TemporalReceptiveField(torch.nn.Module):
     ):
         """Creates ``n_scales`` temporal receptive fields for arbitrary n-dimensional inputs.
         The scale spaces are selected in a range of [min_scale, max_scale] using an exponential distribution, scattered using ``torch.linspace``.
-        Arguments:
-            shape (torch.Size): The shape of the incoming tensor
+
+        Parameters:
+            shape (torch.Size): The shape of the incoming tensor, where the first dimension denote channels
+            n_scales (int): The number of temporal scale spaces to iterate over.
+            activation (SNNCell): The activation neuron. Defaults to LIBoxCell
+            activation_state_map (Callable): A function that takes a tensor and provides a neuron parameter tuple.
+                Required if activation is changed, since the default behaviour provides LIBoxParameters.
             min_scale (float): The minimum scale space. Defaults to 1.
-            max_scale (float): The maximum scale space. Defaults to 32.
+            time_constants (Optional[torch.Tensor]): Hardcoded time constants. Will overwrite the automatically generated, logarithmically distributed scales, if set. Defaults to None.
             dt (float): Neuron simulation timestep. Defaults to 0.001.
         """
         super().__init__()

norse/torch/utils/test/test_nir_export.py
@@ -5,15 +5,14 @@ import norse.torch as norse
 
 
 def test_conv2d():
-    m = norse.SequentialState(
-        torch.nn.Conv2d(1, 2, 3)
-    )
+    m = norse.SequentialState(torch.nn.Conv2d(1, 2, 3))
     graph = norse.to_nir(m, torch.randn(1, 1, 10, 10))
     assert len(graph.nodes) == 3
     assert isinstance(graph.nodes["input"], nir.Input)
     assert isinstance(graph.nodes["0"], nir.Conv2d)
     assert isinstance(graph.nodes["output"], nir.Output)
     assert len(graph.edges) == 2
+
 
 def test_sequential():
     m = norse.SequentialState(
@@ -42,4 +41,3 @@ def test_linear():
     assert len(graph.nodes) == 3
     assert graph.nodes["norse"].weight.shape == (out_features, in_features)
     assert graph.nodes["norse"].bias.shape == m2.bias.shape
-