7208e4d2fa894c0a5e3108b43aa8ea0ddb0e9d8a
Jens E. Pedersen <jensegholm@protonmail.com>
2023-09-27 00:05:35 +0200

    Tested import of nir primitives

--- norse/torch/utils/import_nir.py
+++ norse/torch/utils/import_nir.py
@@ -1,52 +1,73 @@
 import nir
 import nirtorch
+import numpy as np
 import torch
 
 import norse.torch.module.iaf as iaf
 import norse.torch.module.lif_box as lif_box
 import norse.torch.module.lif as lif
 
+import logging
 
-def _import_norse_module(node: nir.NIRNode) -> torch.nn.Module:
+
+def _log_warning(parameter_name: str, expected_value: str, actual_value: str):
+    logging.warning(
+        """The parameter {} is expected to be set to {}, but was found to diverge (with mean {}).
+Please read the Norse documentation for more information.
+This warning can be turned off by setting from_nir(..., ignore_warnings=True)
+"""
+    )
+
+
+def _is_identity(array: np.ndarray, expected_value: float):
+    return np.allclose(array, expected_value)
+
+
+def _import_norse_module(
+    node: nir.NIRNode, ignore_warnings: bool = False
+) -> torch.nn.Module:
     if isinstance(node, nir.Affine):
         return torch.nn.Linear(node.weight.shape[1], node.weight.shape[0])
     if isinstance(node, nir.Conv2d):
         return torch.nn.Conv2d(
-            node.weight.shape[1],
-            node.weight.shape[0],
-            node.kernel_size,
-            node.stride,
-            node.padding,
-            node.dilation,
-            node.groups,
+            in_channels=node.weight.shape[1],
+            out_channels=node.weight.shape[0],
+            kernel_size=node.weight.shape[-2:],
+            stride=node.stride,
+            padding=node.padding,
+            dilation=node.dilation,
+            groups=node.groups,
         )
     if isinstance(node, nir.Linear):
         return torch.nn.Linear(
             node.weight.shape[1], torch.zeros_like(node.weight.shape[0])
         )
     if isinstance(node, nir.IF):
-        return iaf.IAFCell(
-            tau=1 / node.tau,  # Invert time constant
-            v_threshold=node.v_threshold,
-            v_leak=node.v_leak,
-            r=torch.ones(node.v_threshold.shape),
-        )
+        if not _is_identity(node.r, 1) and not ignore_warnings:
+            _log_warning("r", 1, node.r)
+        return iaf.IAFCell(iaf.IAFParameters(v_th=node.v_threshold))
     if isinstance(node, nir.CubaLIF):
+        if not _is_identity(node.r, 1) and not ignore_warnings:
+            _log_warning("r", 1, node.r)
         return lif.LIFCell(
-            tau_mem=1 / node.tau_mem,  # Invert time constant
-            tau_syn=1 / node.tau_syn,  # Invert time constant
-            v_threshold=node.v_threshold,
-            v_leak=node.v_leak,
-            r=torch.ones_like(node.v_leak),
+            lif.LIFParameters(
+                tau_mem_inv=1 / node.tau_mem,  # Invert time constant
+                tau_syn_inv=1 / node.tau_syn,  # Invert time constant
+                v_th=node.v_threshold,
+                v_leak=node.v_leak,
+            )
         )
     if isinstance(node, nir.LIF):
+        if not _is_identity(node.r, 1) and not ignore_warnings:
+            _log_warning("r", 1, node.r)
         return lif_box.LIFBoxCell(
-            tau=1 / node.tau,  # Invert time constant
-            v_threshold=node.v_threshold,
-            v_leak=node.v_leak,
-            r=torch.ones_like(node.v_leak),
+            lif_box.LIFBoxParameters(
+                tau_mem_inv=1 / node.tau,  # Invert time constant
+                v_th=node.v_threshold,
+                v_leak=node.v_leak,
+            )
         )
 
 
-def from_nir(node: nir.NIRNode) -> torch.nn.Module:
+def from_nir(node: nir.NIRNode, ignore_warnings: bool = False) -> torch.nn.Module:
     return nirtorch.load(node, _import_norse_module)
--- norse/torch/utils/test/test_nir_import.py
+++ norse/torch/utils/test/test_nir_import.py
@@ -5,6 +5,10 @@ import torch
 import norse.torch as norse
 
 
+def _convert_nodes(*args):
+    return norse.from_nir(nir.NIRGraph.from_list(*args))
+
+
 def test_import_linear():
     n = nir.NIRGraph.from_list(
         nir.Input({"input": torch.tensor([2])}),
@@ -12,5 +16,27 @@ def test_import_linear():
         nir.Output({"output": torch.tensor([3])}),
     )
     m = norse.from_nir(n)
-    print(m)
-    # assert isinstance(m, torch.nn.Linear)
+    assert isinstance(m.affine, torch.nn.Linear)
+    m(torch.randn(1, 3))  # Test application
+
+
+def test_import_conv2d():
+    m = _convert_nodes(nir.Conv2d(torch.randn(1, 2, 3, 3), 1, 0, 1, 1, torch.randn(2)))
+    assert isinstance(m.conv2d, torch.nn.Conv2d)
+    m(torch.randn(1, 2, 3, 3))  # Test application
+
+
+def test_import_if():
+    m = _convert_nodes(nir.IF(torch.randn(10), torch.randn(10)))
+    assert isinstance(getattr(m, "if"), norse.IAFCell)
+    m(torch.randn(1, 10))  # Test application
+
+def test_import_lif():
+    m = _convert_nodes(nir.LIF(torch.randn(10), torch.ones(10), torch.randn(10), torch.randn(10)))
+    assert isinstance(m.lif, norse.LIFBoxCell)
+    m(torch.randn(1, 10))  # Test application
+
+def test_import_cubalif():
+    m = _convert_nodes(nir.CubaLIF(torch.randn(10), torch.randn(10), torch.ones(10), torch.randn(10), torch.randn(10)))
+    assert isinstance(m.cubalif, norse.LIFCell)
+    m(torch.randn(1, 10))  # Test application
