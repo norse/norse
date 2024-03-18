Fixed scale normalization and simplified derivative (#384)

norse/torch/functional/test/test_receptive_field.py
@@ -3,22 +3,53 @@ from norse.torch.functional import receptive_field
 
 
 def test_spatial_rf():
-    rf = receptive_field.spatial_receptive_field(1, 1, 0, size=9)
+    rf = receptive_field.spatial_receptive_field(1, 0, 1, size=9)
     assert rf.shape == (9, 9)
+
+
+def test_gaussian_kernel_backwards():
+    angle = torch.tensor([0.0], requires_grad=True)
+    ratio = torch.tensor([2.0], requires_grad=True)
+    c = receptive_field.covariance_matrix(ratio, 1 / ratio, angle)
+    kernel = receptive_field.gaussian_kernel(9, c)
+    assert kernel.shape == (9, 9)
+    assert kernel.requires_grad
+
+    kernel.sum().backward()
+    assert angle.grad is not None and angle.grad != 0
+    assert ratio.grad is not None and ratio.grad != 0
 
 
 def test_spatial_rf_backward():
     angle = torch.tensor([0.0], requires_grad=True)
-    ratio = torch.tensor([1.0], requires_grad=True)
-    scale = torch.tensor([5.0], requires_grad=True)
+    ratio = torch.tensor([2.0], requires_grad=True)
+    scale = torch.tensor([1.0], requires_grad=True)
     rf = receptive_field.spatial_receptive_field(scale, angle, ratio, 9)
     assert rf.shape == (9, 9)
     assert rf.requires_grad
 
     rf.sum().backward()
-    assert angle.grad is not None
-    assert ratio.grad is not None
-    assert scale.grad is not None
+    assert angle.grad is not None and angle.grad != 0
+    assert ratio.grad is not None and ratio.grad != 0
+    assert scale.grad is not None and scale.grad != 0
+
+
+def test_covariance_backward():
+    ratio = torch.tensor([2.0], requires_grad=True)
+    angle = torch.tensor([3.0], requires_grad=True)
+    c = receptive_field.covariance_matrix(ratio, 1 / ratio, angle)
+    c.sum().backward()
+    assert ratio.grad is not None and ratio.grad != 0
+    assert angle.grad is not None and angle.grad != 0
+
+
+def test_derive_backward():
+    field = torch.randn(9, 9, requires_grad=True)
+    angle = torch.tensor([0.0], requires_grad=True)
+    out = receptive_field.derive_kernel(field, angle)
+    out.sum().backward()
+    assert angle.grad is not None and angle.grad != 0
+    assert field.grad is not None and not torch.all(torch.eq(field.grad, 0))
 
 
 def test_spatial_parameters_derivative():

norse/torch/utils/pytree.py
@@ -13,7 +13,7 @@ from torch.utils._pytree import (
     PyTree,
     _namedtuple_flatten,
     _namedtuple_unflatten,
-    _register_pytree_node,
+    register_pytree_node,
     tree_map,
 )
 
@@ -52,7 +52,7 @@ def tree_map_only(ty: Type[T], fn: Callable[[T], Any], pytree: PyTree) -> PyTree
 
 
 def register_tuple(typ: Any):
-    _register_pytree_node(typ, _namedtuple_flatten, _namedtuple_unflatten)
+    register_pytree_node(typ, _namedtuple_flatten, _namedtuple_unflatten)
 
 
 class MultipleInheritanceNamedTupleMeta(NamedTupleMeta):