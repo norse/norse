fix: Remove factor of dt from time constants

norse/torch/utils/export_nir.py
@@ -1,4 +1,3 @@
-from functools import partial
 from typing import Optional
 
 import torch
@@ -12,7 +11,7 @@ import norse.torch.module.lif_box as lif_box
 
 
 def _extract_norse_module(
-    module: torch.nn.Module, dt: float = 0.001
+    module: torch.nn.Module
 ) -> Optional[nir.NIRNode]:
     if isinstance(module, torch.nn.Conv2d):
         return nir.Conv2d(
@@ -26,22 +25,22 @@ def _extract_norse_module(
         )
     if isinstance(module, lif.LIFCell):
         return nir.CubaLIF(
-            tau_mem=dt / module.p.tau_mem_inv.detach(),  # Invert time constant
-            tau_syn=dt / module.p.tau_syn_inv.detach(),  # Invert time constant
+            tau_mem=1 / module.p.tau_mem_inv.detach(),  # Invert time constant
+            tau_syn=1 / module.p.tau_syn_inv.detach(),  # Invert time constant
             v_threshold=module.p.v_th.detach(),
             v_leak=module.p.v_leak.detach(),
             r=torch.ones_like(module.p.v_leak.detach()),
         )
     if isinstance(module, lif_box.LIFBoxCell):
         return nir.LIF(
-            tau=dt / module.p.tau_mem_inv.detach(),  # Invert time constant
+            tau=1 / module.p.tau_mem_inv.detach(),  # Invert time constant
             v_threshold=module.p.v_th.detach(),
             v_leak=module.p.v_leak.detach(),
             r=torch.ones_like(module.p.v_leak.detach()),
         )
     if isinstance(module, leaky_integrator_box.LIBoxCell):
         return nir.LI(
-            tau=dt / module.p.tau_mem_inv.detach(),  # Invert time constant
+            tau=1 / module.p.tau_mem_inv.detach(),  # Invert time constant
             v_leak=module.p.v_leak.detach(),
             r=torch.ones_like(module.p.v_leak.detach()),
         )
@@ -65,11 +64,10 @@ def to_nir(
     module: torch.nn.Module,
     sample_data: torch.Tensor,
     model_name: str = "norse",
-    dt: float = 0.001,
 ) -> nir.NIRNode:
     return extract_nir_graph(
         module,
-        partial(_extract_norse_module, dt=dt),
+        _extract_norse_module,
         sample_data,
         model_name=model_name,
     )