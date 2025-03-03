5aafc530f20d34570a027f3864e6af04f83223a1
Jens E. Pedersen <jens@jepedersen.dk>
2025-03-03 16:55:26 +0100

    Renamed dt -> time_scaling_factor

--- norse/torch/utils/export_nir.py
+++ norse/torch/utils/export_nir.py
@@ -28,7 +28,7 @@ def _align_shapes(
 
 
 def _extract_norse_module(
-    module: torch.nn.Module, dt: float = 1
+    module: torch.nn.Module, time_scaling_factor: float = 1
 ) -> Optional[nir.NIRNode]:
     if isinstance(module, torch.nn.Conv2d):
         return nir.Conv2d(
@@ -43,11 +43,11 @@ def _extract_norse_module(
     if isinstance(module, lif.LIFCell):
         shape = module.p.tau_mem_inv.shape
         return nir.CubaLIF(
-            tau_mem=dt
+            tau_mem=time_scaling_factor
             / _align_shapes(
                 module.p.tau_mem_inv.detach(), shape, "tau_syn"
             ),  # Invert time constant
-            tau_syn=dt
+            tau_syn=time_scaling_factor
             / _align_shapes(
                 module.p.tau_syn_inv.detach(), shape, "tau_syn"
             ),  # Invert time constant
@@ -59,7 +59,7 @@ def _extract_norse_module(
     if isinstance(module, lif_box.LIFBoxCell):
         shape = module.p.tau_mem_inv.shape
         return nir.LIF(
-            tau=dt
+            tau=time_scaling_factor
             / _align_shapes(
                 module.p.tau_mem_inv.detach(), shape, "tau"
             ),  # Invert time constant
@@ -70,7 +70,7 @@ def _extract_norse_module(
     if isinstance(module, leaky_integrator_box.LIBoxCell):
         shape = module.p.tau_mem_inv.shape
         return nir.LI(
-            tau=dt
+            tau=time_scaling_factor
             / _align_shapes(
                 module.p.tau_mem_inv.detach(), shape, "tau_mem_inv"
             ),  # Invert time constant
@@ -97,21 +97,22 @@ def to_nir(
     module: torch.nn.Module,
     sample_data: torch.Tensor,
     model_name: str = "norse",
-    dt: float = 1,
+    time_scaling_factor: float = 1,
 ) -> nir.NIRNode:
     """Converts a Norse module to a NIR graph.
 
     Args:
-        module: Norse module to convert
-        sample_data: Sample data to infer input shape
-        model_name: Name of the model
-        dt: Integration time step to use when converting neurons. This parameter should be set to 1
-            to retain the dynamics of the neuron equation. However, if your network has been trained
-            with a different dt, you should set this parameter to the value used during training.
+        module (torch.nn.Module): Norse module to convert
+        sample_data (torch.Tensor): Sample data to infer input shape
+        model_name (str): Name of the model
+        time_scaling_factor (float): Integration time step to use when converting neurons. This parameter
+            defaults to 1 which retains the dynamics of the neuron equation. However, if your network has
+            been trained with a different dt, this scaling factor can re-scale the network dynamics as
+            needed.
     """
     return extract_nir_graph(
         module,
-        partial(_extract_norse_module, dt=dt),
+        partial(_extract_norse_module, time_scaling_factor=time_scaling_factor),
         sample_data,
         model_name=model_name,
     )
