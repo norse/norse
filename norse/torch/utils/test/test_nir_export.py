af8294fa9c15363205f662a1e3fcc732d727ad41
Ben Kroehs <119967834+benkroehs@users.noreply.github.com>
2026-02-09 20:55:08 +0100

    feat(nir): Export parameters as numpy arrays (#435)

--- norse/torch/utils/test/test_nir_export.py
+++ norse/torch/utils/test/test_nir_export.py
@@ -1,5 +1,6 @@
 import torch
 import nir
+import numpy as np
 
 import norse.torch as norse
 
@@ -54,11 +55,15 @@ def test_li_varying_time_scaling_factor():
     m = norse.LIBoxCell(p)
     node = norse.to_nir(m, time_scaling_factor=1.0)
     assert isinstance(node, nir.LI)
-    assert torch.allclose(node.tau, 1.0 / p.tau_mem_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
+    assert np.allclose(node.tau, 1.0 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
     node = norse.to_nir(m, time_scaling_factor=0.5)
-    assert torch.allclose(node.tau, 0.5 / p.tau_mem_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
+    assert np.allclose(node.tau, 0.5 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
 
 
 def test_lif_sequential():
@@ -83,13 +88,19 @@ def test_lif_varying_time_scaling_factor():
     m = norse.LIFCell(p)
     node = norse.to_nir(m, time_scaling_factor=1.0)
     assert isinstance(node, nir.CubaLIF)
-    assert torch.allclose(node.tau_mem, 1.0 / p.tau_mem_inv)
-    assert torch.allclose(node.tau_syn, 1.0 / p.tau_syn_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
+    assert np.allclose(node.tau_mem, 1.0 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau_mem, np.ndarray)
+    assert np.allclose(node.tau_syn, 1.0 / p.tau_syn_inv.numpy())
+    assert isinstance(node.tau_syn, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
     node = norse.to_nir(m, time_scaling_factor=0.5)
-    assert torch.allclose(node.tau_mem, 0.5 / p.tau_mem_inv)
-    assert torch.allclose(node.tau_syn, 0.5 / p.tau_syn_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
+    assert np.allclose(node.tau_mem, 0.5 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau_mem, np.ndarray)
+    assert np.allclose(node.tau_syn, 0.5 / p.tau_syn_inv.numpy())
+    assert isinstance(node.tau_syn, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
 
 
 def test_lif_box_varying_time_scaling_factor():
@@ -99,11 +110,15 @@ def test_lif_box_varying_time_scaling_factor():
     m = norse.LIFBoxCell(p)
     node = norse.to_nir(m, time_scaling_factor=1.0)
     assert isinstance(node, nir.LIF)
-    assert torch.allclose(node.tau, 1.0 / p.tau_mem_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
+    assert np.allclose(node.tau, 1.0 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
     node = norse.to_nir(m, time_scaling_factor=0.5)
-    assert torch.allclose(node.tau, 0.5 / p.tau_mem_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
+    assert np.allclose(node.tau, 0.5 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
 
 
 def test_lif_box_v_reset():
@@ -115,9 +130,12 @@ def test_lif_box_v_reset():
     m = norse.LIFBoxCell(p)
     node = norse.to_nir(m, time_scaling_factor=1.0)
     assert isinstance(node, nir.LIF)
-    assert torch.allclose(node.tau, 1.0 / p.tau_mem_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
-    assert torch.allclose(node.v_reset, p.v_reset)
+    assert np.allclose(node.tau, 1.0 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
+    assert np.allclose(node.v_reset, p.v_reset.numpy())
+    assert isinstance(node.v_reset, np.ndarray)
 
 
 def test_lif_box_v_reset_default():
@@ -127,6 +145,9 @@ def test_lif_box_v_reset_default():
     m = norse.LIFBoxCell(p)
     node = norse.to_nir(m, time_scaling_factor=1.0)
     assert isinstance(node, nir.LIF)
-    assert torch.allclose(node.tau, 1.0 / p.tau_mem_inv)
-    assert torch.allclose(node.v_leak, p.v_leak)
-    assert torch.allclose(node.v_reset, torch.zeros_like(p.v_leak))
+    assert np.allclose(node.tau, 1.0 / p.tau_mem_inv.numpy())
+    assert isinstance(node.tau, np.ndarray)
+    assert np.allclose(node.v_leak, p.v_leak.numpy())
+    assert isinstance(node.v_leak, np.ndarray)
+    assert np.allclose(node.v_reset, np.zeros_like(p.v_leak.numpy()))
+    assert isinstance(node.v_reset, np.ndarray)
